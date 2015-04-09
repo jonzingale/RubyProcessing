010cb7e10829571397d5f47c96620c28a7a9c2c8
Jonathan Zingale <jonzingale@gmail.com>
2015-04-09 11:47:00 -0600

    cleaner rsa_groups

--- rsa_groups.rb
+++ rsa_groups.rb
@@ -1,38 +1,39 @@
-# Todo: auto rescaling; change on the fly
+# This ruby-processing file generates the group
+# multiplication table for a group of units of n,
+# given an integer n.
+
+	# Todo: better auto rescaling; change n on keystroke.
 	def setup
-		text_font create_font("SanSerif",10);
-		square = [800] * 2  + [P3D]
-		@w,@h = [square[0]/2] * 2
-		size(*square) ; background(0)
-		frame_rate 3 ; colorMode(HSB,360,100,100)
-		@table = color_it(rsa_group(60))
+		background(0)
+		zahlen = 121# <-- change integer here.
+		@table = color_it(rsa_group(zahlen))
+		square = [1080] * 2 ; size(*square)
+		frame_rate 1 ; colorMode(HSB,360,100,100)
 	end
 
 	def pretty_print(board)
-		e_size = 30 # augmentation size
+		scale = width/board[0].length*0.9
 		board.each_with_index do |row,c_dex|
 			row.each_with_index do |c,r_dex|
-				x,y = [r_dex,c_dex].map{|i|i*e_size+100}
-				fill(c,100,100) ; ellipse(x,y,e_size,e_size)
+				x,y = [r_dex,c_dex].map{|i|i*scale+50}
+				fill(c,100,100) ; ellipse(x,y,scale,scale)
 			end
 		end
 	end
 
 	def rsa_group(int)
 		non_factors = (1...int).select{|i|i.gcd(int)==1}
-		pairs = non_factors.inject([]){|is,i| is<<(non_factors).map{|j|i*j % int} }
- 		# pairs.map{|p|puts "#{p}"}
+		non_factors.inject([]){|is,i| is << (non_factors).map{|j| i*j%int} }
 	end
 
 	def color_it(table)
 		roots_color = (1..table.length).map{|n|360*n/(table.length)}
 		apply_color = table.map do |k|
-			keyed = k.zip(table[0]).sort
-			keyed.zip(roots_color).sort_by{|i|i[0][1]}.map(&:last)
+			keyed = k.zip(table[0]).sort.zip(roots_color)
+			keyed.sort_by{|i|i[0][1]}.map(&:last)
 		end
 	end
 
 	def draw
-		# rotateX(PI/3.0)
 		pretty_print(@table)
 	end
