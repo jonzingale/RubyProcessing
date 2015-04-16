1825a84a01277c474b2fae6ed9e37a14de51cb0e
Jon Zingale <jon@jackrabbitsystems.com>
2015-04-15 23:45:36 -0600

    walker based on center of mass in rgb space added

--- color_sampler.rb
+++ color_sampler.rb
@@ -12,8 +12,8 @@
 		@img.loadPixels()
 		background(20) ; frame_rate 30
 		# width, height
-		size(1920,1080) #JackRabbit
-		# size(1400,1000) #HOME
+		# size(1920,1080) #JackRabbit
+		size(1400,1000) #HOME
 		@w,@h = [width,height].map{|i|i/2.0}
   	@walker = [@w+200,@h-200] ; @m = [235,18,85]
 	end
@@ -43,33 +43,46 @@
 		Math.sqrt(norm.inject :+)
 	end
 	
-	def walker_z(p=@walker) # pair tends toward @m
-		pair = @low_pair.nil? ? @walker : @low_pair
+		# better neighborhoods and guesses
+		# sum along rays?
+		# spider_plant like sporing?
+		# diff of the diff?
+		# remember the last n and if jostling make e_ball smaller.
+###########
+	def walker_y(p=@walker) # pair tends toward @m, 
+		# center of mass, not very robust, bug when all nears are the same.
 		pair2 = @high_pair.nil? ? @walker : @high_pair
+		triple = rgb_converter(*pair2) ; e_ball = rand(100) # <- a fun idea
+
+		neighborhood = rootsUnity(17)+[[0,0]]
+		rgb_weights = neighborhood.map do |s|
+			unital_color = pair2.zip(s).map{|p,r|p+r*e_ball}
+			diff([rgb_converter(*unital_color), @m])
+		end
+
+		weight_total = rgb_weights.inject(0,:+)
+		aTTw = neighborhood.zip(rgb_weights).inject([]) do |s,abw| 
+			s << abw[0].map{|i|i*abw[1]}
+		end.transpose.map{|i|i.inject :+}
+		norm = aTTw.map{|i|-i*5/weight_total}
+		@high_pair = norm.zip(pair2).map{|rp|rp.inject :+}
+
+		fill(255,255,255) ; text('y',*@high_pair) #; ellipse(*@high_pair,10,10)
+	end
+
+	def walker_z(p=@walker) # pair tends toward @m, best so far
+		pair = @low_pair.nil? ? @walker : @low_pair
 		triple = rgb_converter(*pair)
 		e_ball = rand(100) # <- a fun idea
-
 
 		@low_pair = (rootsUnity(17)+[[0,0]]).min_by do |s|
 			unital_color = pair.zip(s).map{|p,r|p+r*e_ball}
 			diff([rgb_converter(*unital_color), @m])
 		end.zip(pair).map{|rp|rp.inject :+}
 
-		fill(0,0,0) ; ellipse(*@low_pair,10,10)
-		# better neighborhoods and guesses
-		# sum along rays?
-		# spider_plant like sporing?
-		# diff of the diff?
-		# remember the last n and if jostling make e_ball smaller.
-
-		# complementary white dot, kinda ridiculous
-		# @high_pair = rootsUnity(17).max_by do |s|
-		# 	unital_color = pair2.zip(s).map{|p,r|p+r*e_ball}
-		# 	diff([rgb_converter(*unital_color), @m])
-		# end.zip(pair2).map{|rp|rp.inject :+}
-		# fill(255,255,255) ; ellipse(*@high_pair,10,10)
+		fill(0,0,0) ; text('z',*@low_pair)# ; ellipse(*@low_pair,10,10)
 	end
-
+##########
 	def draw
 		image(@jmg,10,10) # kolmogorov
 		pushMatrix # CIE_1931
@@ -77,7 +90,7 @@
 		popMatrix	
 
 		# color ellipse
-		fill(*@m,255) ; ellipse(width-400,height-300,200,200);
+		fill(*@m,250) ; ellipse(width-400,height-300,200,200);
 		fill(255,255,255) ; text("#{@m}",width-480,height-250)
 
 		# best guess ellipse
@@ -86,4 +99,5 @@
 		fill(255,255,255) ; text("#{it}",width-340,height-350)
 
 		walker_z
+		walker_y
 	end
