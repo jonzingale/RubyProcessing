ed9068d8d73dc0bb2cd6cb356f7d656e25b446cf
Jon Zingale <jon@jackrabbitsystems.com>
2015-04-19 19:49:14 -0600

    some ideas for tesssacts

--- color_sampler.rb
+++ color_sampler.rb
@@ -7,23 +7,25 @@
 #    might not be what I want as it matches
 #    luminosity most likely.
 # If not close enough, give up and walk
+# a second guesser that emulates shaking the mouse!!
+
 
 	def setup
 		text_font create_font("SanSerif",25) ; no_stroke
-		# @img = loadImage("/Users/Jon/Desktop/CIE_1931.png")
+		@img = loadImage("/Users/Jon/Desktop/CIE_1931.png")
 		@jmg = loadImage("/Users/Jon/Desktop/scans/imgo_daniel.jpeg")
-		@img = loadImage("/Users/Jon/Desktop/scans/apollonius.jpg");
-		@img.loadPixels()
-		background(20) ; frame_rate 30
+		# @img = loadImage("/Users/Jon/Desktop/scans/apollonius.jpg");
+
 		# width, height
-		size(1920,1080) #JackRabbit
-		# size(1400,1000) #HOME
+		size(1400,1000) #HOME
+		# size(1920,1080) #JackRabbit
+		background(20) ; frame_rate 30
 		@w,@h = [width,height].map{|i|i/2.0}
-  	@walker = [@w+200,@h-200] ; @m = [235,18,85]
+  	@walker = [@w+400,@h-200] ; @m = [235,18,85] ; @i = 0
 	end
 
 	def trigs(theta)#:: Theta -> R2
-	  @cos,@sin = %w(cos sin).map{|s| eval("Math.#{s} #{theta}")}
+	  %w(cos sin).map{|s| eval("Math.#{s} #{theta}")}
 	end
 
 	def rootsUnity(numbre)#::Int -> [trivalStar]
@@ -57,12 +59,12 @@
 ###########
 	def walker_y(p=@walker) # center of mass 
 		# center of mass, not very robust, bug when all nears are the same.
-		pair2 = @high_pair.nil? ? @walker : @high_pair
-		triple = rgb_converter(*pair2) ; e_ball = rand(100) # <- a fun idea
+		pair = @high_pair.nil? ? @walker : @high_pair
+		e_ball = rand(100) # <- novel idea
 
 		neighborhood = rootsUnity(17)+[[0,0]]
 		rgb_weights = neighborhood.map do |s|
-			unital_color = pair2.zip(s).map{|p,r|p+r*e_ball}
+			unital_color = pair.zip(s).map{|p,r|p+r*e_ball}
 			diff([rgb_converter(*unital_color), @m])
 		end
 
@@ -71,14 +73,13 @@
 			s << abw[0].map{|i|i*abw[1]}
 		end.transpose.map{|i|i.inject :+}
 		norm = aTTw.map{|i|-i*10/weight_total}
-		@high_pair = norm.zip(pair2).map{|rp|rp.inject :+}
+		@high_pair = norm.zip(pair).map{|rp|rp.inject :+}
 
 		fill(255,255,255) ; text('y',*@high_pair)
 	end
 
 	def walker_z(p=@walker) # least difference
 		pair = @low_pair.nil? ? @walker : @low_pair
-		triple = rgb_converter(*pair)
 		e_ball = rand(100) # <- novel idea
 
 		@low_pair = (rootsUnity(17)+[[0,0]]).min_by do |s|
@@ -88,23 +89,57 @@
 
 		fill(0,0,0) ; text('z',*@low_pair)
 	end
+
+	def walker_w(p=@walker) # modifier
+		pair = @mod_pair.nil? ? @walker : @mod_pair
+		e_ball = rand(300) # <- novel idea
+
+		@mod_pair = (rootsUnity(17)+[[0,0]]).min_by do |s|
+			unital_color = pair.zip(s).map{|p,r|p+r*e_ball}
+			diff([rgb_converter(*unital_color), @m])
+		end.zip(pair).map{|rp|rp.inject :+}
+
+# try using get. then one can 'get' the whole screen with no params
+# saving this may make it possible to color!!!
+
+		# loadPixels
+			# set(*@mod_pair,0) #; x,y = @mod_pair.map(&:to_i)
+		# 	it = pixels[y*width+x]
+		# 	it = 0
+		# 	save('/Users/Jon/Desktop/test.png')
+		# 	@loaded = loadImage("/Users/Jon/Desktop/test.png")
+		# updatePixels
+		# text("#{it} is awesome",200,200)
+		fill(0,0,0) ; text('w',*@mod_pair)
+	end
+#######
+	def images
+		if @i < 1
+			image(@jmg,10,10) # left picture
+			scale(0.3) ; image(@img, 1.8*width, 10) ; scale(10/3.0) #CIE
+			# scale(0.6) ; image(@img, width-450, 10) ; scale(5/3.0) #APOLLONIUS
+			save('/Users/Jon/Desktop/test.png')
+			@loaded = loadImage("/Users/Jon/Desktop/test.png")
+		else
+			image(@loaded,0,0)
+		end
+	end
 ##########
 	def draw
-		image(@jmg,10,10) # kolmogorov
-		pushMatrix # CIE_1931
-			# scale(0.3) ; image(@img, 1.7*width, 10) #CIE
-			scale(0.6) ; image(@img, width-450, 10) #APOLLONIUS
-		popMatrix	
+		images ; @i = 1
+		@mod_pair.nil? ? nil : set(*@mod_pair,0)
+		@loaded = get
 
 		# color ellipse
-		fill(*@m,250) ; ellipse(width-400,height-300,200,200);
-		fill(255,255,255) ; text("#{@m}",width-480,height-250)
+		fill(*@m,250) ; ellipse(width-300,height-300,200,200);
+		fill(255,255,255) ; text("#{@m}",width-480,height-150)
 
 		# best guess ellipse
 		it = @low_pair.nil? ? @m : rgb_converter(*@low_pair)
-		fill(*it,200) ; ellipse(width-300,height-300,200,200)
-		fill(255,255,255) ; text("#{it}",width-340,height-350)
+		fill(*it,200) ; ellipse(width-200,height-300,200,200)
+		fill(255,255,255) ; text("#{it}",width-340,height-250)
 
 		walker_z
 		walker_y
+		walker_w
 	end
