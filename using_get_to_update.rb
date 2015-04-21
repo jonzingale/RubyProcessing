eda40750c250c11aa5a5056557861c91e3cb1423
Jonathan Zingale <jonzingale@gmail.com>
2015-04-21 15:59:20 -0600

    s

--- using_get_to_update.rb
+++ using_get_to_update.rb
@@ -1,21 +1,22 @@
+# class UsingGetToUpdate
 	def setup
 		text_font create_font("SanSerif",25) ; no_stroke
 		@img = loadImage("/Users/Jon/Desktop/CIE_1931.png")
 		@jmg = loadImage("/Users/Jon/Desktop/scans/imgo_daniel.jpeg")
 
 		# width, height
-		# size(1400,1000) #HOME
-		size(1920,1080) #JackRabbit
+		size(1400,1000) #HOME
+		# size(1920,1080) #JackRabbit
 		background(20) ; frame_rate 30
 		@w,@h = [width,height].map{|i|i/2.0}
   	@walker = [@w+400,@h-200] ; @m = [235,18,85] ; @i = 0
 	end
 
-def trigs(theta) ; %w(cos sin).map{|s| eval("Math.#{s} #{theta}")} ; end
-def rootsUnity(numbre) ; (0...numbre).map{|i|trigs(i*2*PI/numbre)} ; end
-def	mouseMoved ; @m = rgb_converter(mouseX,mouseY) ; end
-def euclidean(w) ; Math.sqrt(w.transpose.map{|p|p.inject(:-)**2}.inject :+) ; end
-def rgb_converter(m=0,n=0) ; c,k = [256,get(m,n)] ; [c+k/(c**2),k/c%c,k%c] ; end
+	def trigs(theta) ; %w(cos sin).map{|s| eval("Math.#{s} #{theta}")} ; end
+	def rootsUnity(numbre) ; (0...numbre).map{|i|trigs(i*2*PI/numbre)} ; end
+	def	mouseMoved ; @m = rgb_converter(mouseX,mouseY) ; end
+	def euclidean(w) ; Math.sqrt(w.transpose.map{|p|p.inject(:-)**2}.inject :+) ; end
+	def rgb_converter(m=0,n=0) ; c,k = [256,get(m,n)] ; [c+k/(c**2),k/c%c,k%c] ; end
 
 	def walker_w(p=@walker) # modifier: leaves a trail of black
 		pair = @mod_pair.nil? ? @walker : @mod_pair
@@ -54,3 +55,4 @@ def rgb_converter(m=0,n=0) ; c,k = [256,get(m,n)] ; [c+k/(c**2),k/c%c,k%c] ; end
 
 		text("#{rgb_converter(*@mod_pair)}",200,200)
 	end
+# end
