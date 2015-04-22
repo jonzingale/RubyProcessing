e3675440a134a9bc9e10049fbf2315c36510626f
Jon Zingale <jon@jackrabbitsystems.com>
2015-04-21 22:29:13 -0600

    a good run toward 4-d rotations

--- test_ruby.rb
+++ test_ruby.rb
@@ -8,36 +8,42 @@ require 'matrix'
 	VECT = Vector.elements([1,2,3])
 	BECT = Vector.elements([4,5,6])
 	@w,@h = [WIDTH,HEIGHT].map{|i|i/2.0}
-	@rand_c = (0..2).map{rand(255)} ; @i = 0
+	@rand_c = (0..2).map{rand(255)} ;  @i, @j, @k = [0]*3
 	PI = 3.1415926
 
 def trigs(theta) ; %w(cos sin).map{|s| eval("Math.#{s} #{theta}")} ; end
 def rootsUnity(numbre) ; (0...numbre).map{|i|trigs(i*2*PI/numbre)} ; end
 
 def vects(d)
-	Matrix.rows((0...2**d).map{|i|("%03d"%i.to_s(2)).split('').map(&:to_i)}) * 300
-end
-
-def rotation_tranny
-	sin ,cos = trigs(@i*2*PI)
-	# sc = trigs(@i*2*PI)
-	Matrix.columns([[- sin,cos,0],[cos,sin,0],[0,0,1]])
+  Matrix.rows((0...2**d).map{|i|("%0#{d}d"%i.to_s(2)).split('').map(&:to_i)}) * 300
 end
 
 	def process
-		@i+=0.01
-	
-		shed = (rotation_tranny * VECT * 100).to_a.map(&:floor).take(2)
-		translate = shed.zip([@w,@h]).map{|a,b|a+b}
+while @i > -1
+	# @i+=0.01 ; @j+=0.02 ; @k+=0.04
+	@i+=0.01 ; @j+=0.5 ; @k+=0.5
+	sin1,cos1 = trigs(@i*2*PI)
+	sin2,cos2 = trigs(@j*2*PI)
+	sin3,cos3 = trigs(@k*2*PI) 
 
-		cube = vects(3).transpose
-		turn = rotation_tranny * cube
-	
-		idea = turn.transpose.to_a.map{|i|i.take(2).map(&:floor)}
-		translate = idea.map{|t|t.zip([@w,@h]).map{|a,b|a+b}}
+	# translate?
+	rots_3 = Matrix.columns([[sin1 * cos2,0,0],[0,sin1 * sin2,0],[0,0,cos1]])
+	rots_4 = Matrix.columns([[cos1*sin3,0,0,0],
+													 [0,sin1*sin3,0,0],
+													 [0,0,cos2*cos3,0],
+													 [0,0,0,sin2*cos3]])
 
-	byebug
-		end
+
+	hyper_cube = vects(4).transpose
+	turn = rots_4 * hyper_cube
+
+	idea = turn.transpose.to_a.map{|i|i.take(2).map(&:floor)}
+	translate = idea.map{|t|t.zip([@w,@h]).map{|a,b|a+b}}
+sleep(1)
+	puts "#{translate}"
+end
+
+	end
 
 
 process
