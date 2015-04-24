4d79518b15613dc7c3fcfe1ec9468b4f3d7bedb3
Jonathan Zingale <jonzingale@gmail.com>
2015-04-24 15:48:16 -0600

    yay

--- hyper_cube_2.rb
+++ hyper_cube_2.rb
@@ -2,8 +2,7 @@ require 'matrix'
 # this version has trails!
 BASES = (0...4).map{|i|2**i}.freeze # 1,2,4,8
 ALL_POINTS = (0...2**4).inject([],:<<).freeze
-HEXAGON = Matrix.rows([[1,0,-1,1],[0.5,1,0.5,1],[0,0,0,1],[0,0,0,1]]).freeze
-REDUCTION = Matrix.rows([[1,0,0,0],[0,1,0,0]]).freeze
+REDUCED_HEX = Matrix.rows([[1.0, 0, -1.0, 1], [0.5, 1, 0.5, 1]]).freeze
 def trigs(theta) ; %w(cos sin).map{|s| eval("Math.#{s} #{theta}")} ; end
 
 def tranny
@@ -14,15 +13,13 @@ def tranny
 	# xw_rot = Matrix.rows([[cos,0,0,sin],[0,1,0,0],[0,0,1,0],[-sin,0,0,cos]])
 	# yw_rot = Matrix.rows([[1,0,0,0],[0,cos,0,-sin],[0,0,1,0],[0,sin,0,cos]])
 	# yz_rot = Matrix.rows([[1,0,0,0],[ 0,cos,sin,0],[0,-sin,cos,0],[0,0,0,1]])
-	REDUCTION * HEXAGON * zw_rot * xz_rot * xy_rot
+	REDUCED_HEX * zw_rot * xz_rot * xy_rot
 end
 
 def setup
 	@loaded = loadImage("/Users/Jon/Desktop/test.png")
-
 	size(1920,1080) ; background(20) ; frame_rate 20
 	@w,@h = [width,height].map{|i|i/3.0} ; 	@i,@rand_c = [0,(0..2).map{rand(255)}]
-
 	triangles = ALL_POINTS.inject([]){ |a,i| a+=(0...i).map{ |j| [i,j] } }.freeze
 	edges = triangles.map{|a,b|[a,b] if BASES.include?(a^b)}.compact.freeze
 	@coords = edges.map{|a|a.map{|x|("%04d" % x.to_s(2)).split('').map(&:to_i)}}
--- tesseract_4.rb
+++ tesseract_4.rb
@@ -1,50 +1,33 @@
-# todo: faces, translate off of vertex
 require 'matrix'
-ROOT2 = Math.sqrt(2)/2.0
 BASES = (0...4).map{|i|2**i}.freeze # 1,2,4,8
 ALL_POINTS = (0...2**4).inject([],:<<).freeze
-TRIANGLES = ALL_POINTS.inject([]){ |a,i| a+=(0...i).map{ |j| [i,j] } }.freeze
-EDGES = TRIANGLES.map{|a,b|[a,b] if BASES.include?(a^b)}.compact.freeze
-EDGE_COORDS = EDGES.map{|a|a.map{|x|("%04d" % x.to_s(2)).split('').map(&:to_i)}}
-HEXAGON = Matrix.rows([[1,0,-1,1],[0.5,1,0.5,1],[0,0,0,1],[0,0,0,1]]).freeze
-# Can I modify Edges by Hexagon here?
-
-TRANSLATE = Matrix.rows([[1,0,0,1],[0,1,0,0],[0,0,1,1],[0,0,0,1]])
-UNTRANSLATE = Matrix.rows([[1,0,0,-1],[0,1,0,0],[0,0,1,-1],[0,0,0,1]])
-
-REDUCTION = Matrix.rows([[1,0,0,0],[0,1,0,0]]).freeze
+REDUCED_HEX = Matrix.rows([[1.0, 0, -1.0, 1], [0.5, 1, 0.5, 1]]).freeze
 def trigs(theta) ; %w(cos sin).map{|s| eval("Math.#{s} #{theta}")} ; end
-
-def setup
-	background(20) ; frame_rate 30
-	size(1400,1080) #HOME
-	# size(1920,1080)
-	@w,@h = [width,height].map{|i|i/2.0} ; 	@i = 0
-	@rand_c = (0..2).map{rand(255)}
-end
 
 def tranny
 	sin,cos = trigs(@i*2*PI)
 	xy_rot = Matrix.rows([[cos,sin,0,0],[-sin,cos,0,0],[0,0,1,0],[0,0,0,1]])
 	xz_rot = Matrix.rows([[cos,0,-sin,0],[0,1,0,0],[sin,0,cos,0],[0,0,0,1]])
+	yw_rot = Matrix.rows([[1,0,0,0],[0,cos,0,-sin],[0,0,1,0],[0,sin,0,cos]])
 	# zw_rot = Matrix.rows([[1,0,0,0],[0,1,0,0],[0,0,cos,-sin],[0,0,sin,cos]])
 	# xw_rot = Matrix.rows([[cos,0,0,sin],[0,1,0,0],[0,0,1,0],[-sin,0,0,cos]])
-	yw_rot = Matrix.rows([[1,0,0,0],[0,cos,0,-sin],[0,0,1,0],[0,sin,0,cos]])
 	# yz_rot = Matrix.rows([[1,0,0,0],[ 0,cos,sin,0],[0,-sin,cos,0],[0,0,0,1]])
+	REDUCED_HEX * yw_rot * xz_rot * xy_rot
+end
 
-	# how do i get the translate to work?
-	REDUCTION * yw_rot * xz_rot * xy_rot * HEXAGON
+def setup
+	size(1920,1080) ; background(20) ; frame_rate 20
+	@w,@h = [width,height].map{|i|i/3.0} ; 	@i,@rand_c = [0,(0..2).map{rand(255)}]
+	triangles = ALL_POINTS.inject([]){ |a,i| a+=(0...i).map{ |j| [i,j] } }.freeze
+	edges = triangles.map{|a,b|[a,b] if BASES.include?(a^b)}.compact.freeze
+	@coords = edges.map{|a|a.map{|x|("%04d" % x.to_s(2)).split('').map(&:to_i)}}
 end
 
 def draw
-	@i+=0.003
+	@i+=0.003 ; e_ball = height/10.0
 	no_stroke ; fill(0) ; rect(0,0,width,height)
 	[:fill,:stroke].each{|f|send(f,*@rand_c)}
-
-	# Can I make e_coords a matrix instead of passing individual Vectors?
-	turn_edges = EDGE_COORDS.map{|s|s.map{|v| (tranny*Vector.elements(v)* 100).to_a}}
-
+	turn_edges = @coords.map{|s|s.map{|v| (tranny*Vector.elements(v)*e_ball).to_a}}
 	translate = turn_edges.map{|edge|edge.map{|d,e| [d+@w,e+@h]} }
 	translate.each{|edge|a,b = edge ; line(*a,*b)}
 end
-  
