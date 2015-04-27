e6cb18f5191b9c61ecc151aef85e27916f6fa550
Jonathan Zingale <jonzingale@gmail.com>
2015-04-26 22:39:24 -0600

    linear trannies visualized

--- big_time_trannies.rb
+++ big_time_trannies.rb
@@ -1,27 +1,37 @@
+#todo: 	# partition vectors, given a tranny.
 require 'matrix'
-ALL_POINTS = 2
-
-
-# BASES = (0...4).map{|i|2**i}.freeze # 1,2,4,8
-# ALL_POINTS = (0...2**4).inject([],:<<).freeze
-
 def setup
-  background(20) ; frame_rate 20 ;	size(1400,1080)
-	@w, @h, @i, @rand_c = [width,height].map{|i|i/3.0}+[0,(0..2).map{rand(255)}]
-	# triangles = ALL_POINTS.inject([]){ |a,i| a+=(0...i).map{ |j| [i,j] } }.freeze
-	# edges = triangles.map{|a,b|[a,b] if BASES.include?(a^b)}.compact.freeze
-	# @coords = edges.map{|a|a.map{|x|("%04d" % x.to_s(2)).split('').map(&:to_i)}}
+  background(20) ; frame_rate 1 ; size(1400,1000) #HOME
+	@i, @rand_c =[0,(0..2).map{rand(255)}]
+	@w,@h = [width,height].map{|i|i/10.0}
+	@mid_v = Vector.elements([@w,@h])
 	[:fill,:stroke].each{|f|send(f,*@rand_c)}
 end
 
-def draw
-	# clear ; cos,sin = %w(cos sin).map{|s| eval("Math.#{s} #{(@i+=0.001)*2*PI}")}
-	# rotation = Matrix.rows([[cos**2,cos*sin,-sin,0],[-cos*sin,cos**2,0,-sin]])
+# there are how many trannies?
+def n_ary(n=2,x=0) ; ("%02d" % x.to_s(n)).split('').map(&:to_i) ; end
+def all_vs(p=2) ; (0...p**2).map{|x|Vector.elements(n_ary(p,x))} ; end
 
-	# transform_edges = [0,1].map do |i|
-	# 	(rotation*Matrix.columns(@coords.transpose[i])* height/4.0).transpose.to_a
-	# end.transpose
 
-	# transform_edges.each{|edge|a,b = edge.map{|d,e| [d+@w,e+@h]} ; line(*a,*b)}
+#######Transformations
+def trannies(n=2) ; tranny_classes(n,unit_trans(n)) ; end
+def unit_trans(p=2)
+	all_vs(p).product(all_vs(p)).map{|vs|Matrix.rows(vs)}.select{|m|(m.det)%p==1}
 end
 
+def tranny_classes(k,ts)
+	ts.map do |t|
+		(1...ts.count).map{|i|(t**i).map{|i|i%k} }.take_while{|t|t!=ID}.unshift(ID)
+	end
+end
+#######
+
+
+def draw
+	clear
+	tranny = unit_trans(5)[rand(unit_trans.count)]
+	sources = all_vs(5) ; targets = sources.map{|v| (tranny*v).map{|i|i%5}}
+	edges = sources.zip(targets).map{|a,b|(a.to_a+b.to_a).map{|i|i*200}}
+
+	edges.each{|edge|line(*edge)}
+end
