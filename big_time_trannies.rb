9b2badf8f91caf32ddb13503b51249eaf608050e
Jonathan Zingale <jonzingale@gmail.com>
2015-04-26 18:08:39 -0600

    linear transformations over finite fields

--- big_time_trannies.rb
+++ big_time_trannies.rb
@@ -1,52 +1,27 @@
-
-	# def setup
-	#   size 800, 600, P3D  
-	# end
-
-	def setup
-		# width,height
-		square = [800] * 2  + [P3D]
-		@w,@h = ([square[0]/2] * 2) + [P3D]
-		size(*square)
-		@bs = [height,width].min
-		@i = 0 ; background(0) ; @walk = 0
-		frame_rate 4
-		@colors = (0..3).map{|i|rand(255)}
-		no_fill()
-	end
-
-	def text_block(string='')
-		fill(0,0,0) ; no_stroke
-		rect(@w-40,@h-40,@w+40,@h+40)
-		fill(200, 140, 0);
-		text(string,@w,@h)
-	end
-
-	def draw
-		@colors = (0..2).map{rand(255)}
-		@i += 1 ; fill(*@colors)
-
-		# pushMatrix()
-				# popMatrix()
-
-pushMatrix()
-noStroke();
-lights();
-translate(100+rand(@i % @bs),100+rand(@i % @bs), 0);
-sphere(rand(30));
-popMatrix()
-
-		translate(width/2-rand(@w), height/2-rand(@h)) if (@i % 6) == 0
-
-		translate(width/2, height/2);
-		rotate(2*PI*(@i % 5)/5.0)
-		rotateX(2*PI*(@i % 3)/3.0)
-
-		# rotate(2*PI*(@i % 3)/3.0);
-		rect(-100, -100, 100, 100);
+require 'matrix'
+ALL_POINTS = 2
 
 
-		text_block("#{PI}")
-		text_block("this is the shit!")
-	end
+# BASES = (0...4).map{|i|2**i}.freeze # 1,2,4,8
+# ALL_POINTS = (0...2**4).inject([],:<<).freeze
+
+def setup
+  background(20) ; frame_rate 20 ;	size(1400,1080)
+	@w, @h, @i, @rand_c = [width,height].map{|i|i/3.0}+[0,(0..2).map{rand(255)}]
+	# triangles = ALL_POINTS.inject([]){ |a,i| a+=(0...i).map{ |j| [i,j] } }.freeze
+	# edges = triangles.map{|a,b|[a,b] if BASES.include?(a^b)}.compact.freeze
+	# @coords = edges.map{|a|a.map{|x|("%04d" % x.to_s(2)).split('').map(&:to_i)}}
+	[:fill,:stroke].each{|f|send(f,*@rand_c)}
+end
+
+def draw
+	# clear ; cos,sin = %w(cos sin).map{|s| eval("Math.#{s} #{(@i+=0.001)*2*PI}")}
+	# rotation = Matrix.rows([[cos**2,cos*sin,-sin,0],[-cos*sin,cos**2,0,-sin]])
+
+	# transform_edges = [0,1].map do |i|
+	# 	(rotation*Matrix.columns(@coords.transpose[i])* height/4.0).transpose.to_a
+	# end.transpose
+
+	# transform_edges.each{|edge|a,b = edge.map{|d,e| [d+@w,e+@h]} ; line(*a,*b)}
+end
 
