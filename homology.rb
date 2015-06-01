f563b3bcce60d9155f625cb5b1bfb91a151a1db0
Jonathan Zingale <jonzingale@gmail.com>
2015-06-01 11:00:05 -0600

    cross_products and orientations

--- homology.rb
+++ homology.rb
@@ -4,7 +4,6 @@ SIMPLICES = (0..6).map{|s| eval("SIMPLEX_#{s} = (0..#{s}).to_a")}.freeze
 def setup
 	text_font create_font("SanSerif",40)
   background(20) ; frame_rate 0.5 ; size(1400,1000) #HOME
-
 	@w,@h = [width,height].map{|i|i/5.0}
 	stroke_width(3)
 end
@@ -12,32 +11,32 @@ end
 def abs(i) ; ((i**2)**0.5).to_i ; end
 def is_compact?(simplex) ; del(del(simplex)).flatten.inject(0,:+) == 0 ; end
 
-# 2-simplex
-def split_simplex(simplex,line,p)
-	faces = del(line.zip(simplex-line<<p).flatten)
-	[faces[0],faces[2]]
-end
-
 def del(simplex)
 	simplex[0].is_a?(Array) ? simplex.inject([]){|xs,s| xs + del(s) } :
 	simplex.map.with_index{|s,i| simplex.reject{|x| x == s}.map{|j| j*(-1)**i } }
 end
 
-# 2-D simplices only
-def add_p(simplex,line,p)
-	edge = simplex - line << p
-	del(simplex).inject([]) do |xs,s|
-		cond = s.map{|j| abs(j) } == line
-		cond ? xs + [[s[0], p],[p, s[1]]] : xs << s
-	end + [edge, edge.map{|i|-i}]
+# if two edges have a common vertex
+# name and cut, otherwise don't.
+def split_triangle(e1,e2)
+	if (sig = e1|e2).count == 3 ; hs = Hash.new
+		(0..4).zip(sig+((0..4).to_a-sig)).each{|i,e| hs[i]=e}
+		[[0,3,2],[3,4,2],[3,1,4]].map{|a| a.map{|i| hs[i]}}
+	end
 end
 
-# generalize to n-dimensions
-def n_add_p(simplex,line,p)
-
+# if two edges have a common vertex
+# name and cut, otherwise don't.
+def split_triangle_2(e1,e2)
+	if (sig = e1|e2).count == 3
+		hs = Hash.new ; keys = %w(a b c d e)
+		keys.zip(sig+['p','q']).each{|i,e| hs[i]=e}
+		%w(adc dec dbe).map{|a| a.split('').map{|i| hs[i]}}
+	end
 end
 
 def draw
 	clear ; translate(@w,@h)
-	n_add_p(SIMPLEX_3, SIMPLEX_3.take(2), 99)
+	
+	split_triangle([0,1],[3,1])
 end
--- test_ruby.rb
+++ test_ruby.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 #test ruby
 # require 'ruby-2.0.0-p247'
+
+
+require 'matrix'
 require 'byebug'
 WIDTH = 100 ; HEIGHT = 100
 simplices = (0..6).map{|s| eval("SIMPLEX_#{s} = (0..#{s}).to_a")}.freeze
@@ -8,60 +11,45 @@ simplices = (0..6).map{|s| eval("SIMPLEX_#{s} = (0..#{s}).to_a")}.freeze
 def abs(i) ; ((i**2)**0.5).to_i ; end
 def is_compact?(simplex) ; del(del(simplex)).flatten.inject(0,:+) == 0 ; end
 
-# 2-simplex
-def split_simplex(simplex,line,p)
-	faces = del(line.zip(simplex-line<<p).flatten)
-	[faces[0],faces[2]]
-end
-
 def del(simplex)
 	simplex[0].is_a?(Array) ? simplex.inject([]){|xs,s| xs + del(s) } :
 	simplex.map.with_index{|s,i| simplex.reject{|x| x == s}.map{|j| j*(-1)**i } }
 end
 
-# 2-D simplices only
-def add_p(simplex,line,p)
-	edge = simplex - line << p
-	del(simplex).inject([]) do |xs,s|
-		cond = s.map{|j| abs(j) } == line
-		cond ? xs + [[s[0], p],[p, s[1]]] : xs << s
-	end + [edge, edge.map{|i|-i}]
+# a weird function
+# [:push,:pop].inject([]){|ds,meth| ds << (sig|sig).send(meth)  }
+
+def cross_product(vs,ws)# cross_product needs ruby 2.1 or greater.
+	indices = [0,1,2].map{ |i| [0,1,2].delete_if{|j| i==j }}
+	if [vs,ws].all?{|vect| vect.count == 3 }
+		a, b = [vs,ws].map{|vect| indices.map{|i,j| Vector[vect[i],vect[j]] }}
+		Vector.elements(a.zip(b).map.with_index{|m,i| Matrix.rows(m).det * (-1)**i})
+	end
 end
 
-# generalize to n-dimensions
-def n_add_p(simplex,line,p)
+def oriented?(face,pt)
+	a, b, c, p = (face << pt).map{|a| Vector.elements(a)}
+	cross_product(b-a,c-a).inner_product(p-a) < 0
+end
 
+# if two edges have a common vertex
+# name and cut, otherwise don't.
+def split_triangle(e1,e2)
+	if (sig = e1|e2).count == 3
+		hs = Hash.new ; keys = %w(a b c d e)
+		keys.zip(sig+['p','q']).each{|i,e| hs[i]=e}
+		%w(adc dec dbe).map{|a| a.split('').map{|i| hs[i]}}
+	end
 end
 
 def process
-# del([0,1,3,4]).permutation.to_a.select{|s| s.include?([0,-1,-3]) && s.include?([1,3,4])}.map{|p| puts "#{p}"}.compact
-# [[1, 3, 4], [0, -3, -4], [0, 1, 4], [0, -1, -3]]
-# [[1, 3, 4], [0, -3, -4], [0, -1, -3], [0, 1, 4]]
-# [[1, 3, 4], [0, 1, 4], [0, -3, -4], [0, -1, -3]]
-# [[1, 3, 4], [0, 1, 4], [0, -1, -3], [0, -3, -4]]
-# [[1, 3, 4], [0, -1, -3], [0, -3, -4], [0, 1, 4]]
-# [[1, 3, 4], [0, -1, -3], [0, 1, 4], [0, -3, -4]]
-# [[0, -3, -4], [1, 3, 4], [0, 1, 4], [0, -1, -3]]
-# [[0, -3, -4], [1, 3, 4], [0, -1, -3], [0, 1, 4]]
-# [[0, -3, -4], [0, 1, 4], [1, 3, 4], [0, -1, -3]]
-# [[0, -3, -4], [0, 1, 4], [0, -1, -3], [1, 3, 4]]
-# [[0, -3, -4], [0, -1, -3], [1, 3, 4], [0, 1, 4]]
-# [[0, -3, -4], [0, -1, -3], [0, 1, 4], [1, 3, 4]]
-# [[0, 1, 4], [1, 3, 4], [0, -3, -4], [0, -1, -3]]
-# [[0, 1, 4], [1, 3, 4], [0, -1, -3], [0, -3, -4]]
-# [[0, 1, 4], [0, -3, -4], [1, 3, 4], [0, -1, -3]]
-# [[0, 1, 4], [0, -3, -4], [0, -1, -3], [1, 3, 4]]
-# [[0, 1, 4], [0, -1, -3], [1, 3, 4], [0, -3, -4]]
-# [[0, 1, 4], [0, -1, -3], [0, -3, -4], [1, 3, 4]]
-# [[0, -1, -3], [1, 3, 4], [0, -3, -4], [0, 1, 4]]
-# [[0, -1, -3], [1, 3, 4], [0, 1, 4], [0, -3, -4]]
-# [[0, -1, -3], [0, -3, -4], [1, 3, 4], [0, 1, 4]]
-# [[0, -1, -3], [0, -3, -4], [0, 1, 4], [1, 3, 4]]
-# [[0, -1, -3], [0, 1, 4], [1, 3, 4], [0, -3, -4]]
-# [[0, -1, -3], [0, 1, 4], [0, -3, -4], [1, 3, 4]]
-	split_simplex([0,1,4],[0,4],2)
 
-	# add_p(SIMPLEX_2, SIMPLEX_2.take(2), 99)
+	it = split_triangle([0,1],[1,3])
+	cross = cross_product(Vector[1,0,0],Vector[0,1,0])
+
+	face = [[1,4,6],[7,2,5],[6,1,5]] ; point = [12,5,2]
+	oriented?(face, point)
+byebug
 end
 
 process
