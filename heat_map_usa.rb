f47249bacc18c77ad3d2e487ed6660f5a53eeecd
Jon Zingale <jon@jackrabbitsystems.com>
2015-08-07 23:19:17 -0600

    growing fibers

--- heat_map_usa.rb
+++ heat_map_usa.rb
@@ -7,7 +7,8 @@ require 'nokogiri'
 require 'open-uri'
 require 'mechanize'
 
-	EXP = 2.718281828.freeze
+	USA_MAP = "/Users/Jon/Desktop/usa_map.jpg".freeze
+	USA_MAP_TEMP = '/Users/Jon/Desktop/usa_map_tmp.jpg'.freeze
 	BASE_URL = 'http://forecast.weather.gov/MapClick.php?'.freeze
 	CITY_DATA = [['santa fe','87505',[355,415]],
 							 ['bullhead city','86429',[183,416]],
@@ -28,48 +29,63 @@ require 'mechanize'
 			page = form.submit
 
 			temp = page.at(CURRENT_TEMP_SEL).text.to_i
-			@data << [temp,coords] ; @i = 0
+			@data << [temp,coords]
 		end
 	end
 
 	def setup
 		text_font create_font("SanSerif",20);
-		@img = loadImage("/Users/Jon/Desktop/usa_map.gif")
-		square = [1000, 700] ; size(*square)
+		square = [1000, 700, P3D] ; size(*square)
 		@w,@h = [square[0]/2] * 2 ; background(0)
 		frame_rate 20 ; colorMode(HSB,360,100,100)
+		no_stroke
 
 		@my, @mx = [0,0]
-		scrape_temps
-	end
+		@i, @t = [0] * 2
 
-	def	mouseMoved 
-		x, y = [mouseX,mouseY]
-		@my = y/(height.to_f)*360
-		@mx = x/(width.to_f)*100
+		# border color scale
+		(0..150).each{|i| fill(scale(i),100,100)
+											ellipse(i*9,height,20,20) }
+
+		# can we rotate the pic back?
+		# rotateX(3.1415/3.0)
+		@loaded = loadImage(USA_MAP)
+		image(@loaded,0,0)
+
+		scrape_temps
 	end
 
 	def scale(temp) # linear
 		scale = 360 * ((136-temp)/136.to_f)
 		translate = scale - 82 % 360
 	end	
-	
-	# def scale(temp) ; 360 * Math.log(EXP**((80-temp)/80.0)) ; end
+
+	def counter ; @i = (@i + 1) % 10 ; end
+
+	def images
+		if @i == 0
+			scrape_temps ; @t += 1
+			save(USA_MAP_TEMP)
+
+			# loads, then displays loaded pic.
+			@loaded = loadImage(USA_MAP_TEMP)
+			image(@loaded,0,0)
+		end
+	end
 
 	def draw
-		image(@img,10,10)
+		counter
+
 		@data.each do |temp,coords|
 			hue = scale(temp)
-			stroke_weight(1) ; stroke(0)
 
-			fill(hue,100,100,95) ; rect(*coords,40,40)
+			# add some random walk sway.
+			x, y = coords ; coords = [x, y - @t * 2]
+			fill(hue,100,100,70) ; rect(*coords,10,10)
+
+			# dont let the number get blurry.
 			fill(0) ; text("#{temp}",*coords)
 		end
 
-		text("#{@i}",200,200)
-		no_stroke ; (0..150).each{|i| fill(scale(i),100,100) ; ellipse(i*9,height,20,20)}
-
-		# rescrapes ever 600 frames.
-		# todo: temperature worms.
-		@i += 1 ; scrape_temps if @i % 600 == 0 
+		images
 	end
