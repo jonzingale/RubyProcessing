d3afd1ff016be1f79017406b69b337bf168639e5
Jon Zingale <jon@jackrabbitsystems.com>
2015-08-16 10:44:48 -0600

    tori and eggs

--- hsb_torus.rb
+++ hsb_torus.rb
@@ -1,59 +1,39 @@
 # sprinkle over torus
 # random walk the radius R
+# save as slow frames and edit out to make faster movies.
+
 require 'matrix'
 	def setup
 		frame_rate 7
 		size(800,800)
 		@w,@h = [400] * 2
-		@i, @j, @t = [0] * 3
+		@i, @j = [0] * 2
 
 		colorMode(HSB,360,100,100)
-		text_font create_font("SanSerif",10)
+		@all_coords = (0..10000).map{ sprinkle }
+		@all_diagonals = (0..700).map{ sprinkle_diag }
+	end
 
-		background(0)
-		@all_coords = (0..4000).map{ sprinkle }
-		@all_diagonals = (0..300).map{ sprinkle_diag }
+	# color setting:
+	def set_color(x,y,z,matrix)
+		a, b, c = matrix.to_a.flatten.map{|x| x/(2* PI.to_f)}
+		color = [(c * 1200) + 200, ((a+10) * 20), 100]
+		# color = [c < 0 ? 100 : 200, a < 0 ? 40 : 90 , 100]
+		set(x+@w, z+@h, color(*color))
 	end
 
 	def set_diag_color(x,y,z,matrix)
-		a, b, c = matrix.to_a.flatten
-
-		color = [rand(60),
-						 (a + 2) * 60 ,
-						 100]
-
-		fill(*color)
-		ellipse(x+@w,
-						z+@h,
-					  7, 7)
+		a, b, c = matrix.to_a.flatten.map{|x| x/(2* PI.to_f)}
+		set(x+@w, z+@h, color(0,0,100))
 	end
-
-
-	def set_color(x,y,z,matrix)
-		a, b, c = matrix.to_a.flatten
-
-		color = [c < 0 ? 100 : 200,
-						 a < 0 ? 40 : 90 ,
-						 100]
-
-		fill(*color)
-		ellipse(x+@w,
-						z+@h,
-					  7, 7)
-	end
-
-	def sin_cos(var) ; %w(sin cos).map {|s| Math.send(s, 2 * PI * var) } ; end
-
-	RAD = 1.618.freeze
-	SCALE = (300 / (1 + RAD).to_f).freeze
+	#
 
 	def sprinkle_diag
 		sin_p, cos_p = sin_cos (360 - (rand 360))/360.0 
-		sin_t, cos_t = sin_cos (360 - (rand 360))/360.0 
 
 		x = (RAD + cos_p) * cos_p
 		y = (RAD + cos_p) * sin_p
-		z = sin_p + 0
+		z = sin_p
 
 		Matrix.columns([[x,y,z]])
 	end
@@ -64,15 +44,19 @@ require 'matrix'
 
 		x = (RAD + cos_p) * cos_t
 		y = (RAD + cos_p) * sin_t
-		z = sin_p + 0
+		z = sin_p
 
 		Matrix.columns([[x,y,z]])
 	end
 
+	# RAD = 0 for circle, RAD 1 for apple, RAD = 2 for torus
+	RAD = 2 ; SCALE = (300 / (1 + RAD).to_f).freeze
+	def sin_cos(var) ; %w(sin cos).map {|s| Math.send(s, 2 * PI * var) } ; end
+
 	def draw
 		clear
 
-		cos,sin = %w(cos sin).map{|s| eval("Math.#{s} #{(@i += 0.002)*2*PI}")}
+		cos,sin = %w(cos sin).map{|s| eval("Math.#{s} #{(@i += 0.002)*PI}")}
 		tranny = [[1,0,0],[sin,cos,0],[0,sin,cos]]		
 		rotation = Matrix.rows(tranny)
 
