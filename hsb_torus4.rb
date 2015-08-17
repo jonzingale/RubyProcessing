d011ad6047294d12fc039feceae20c714e513078
Jonathan Zingale <jonzingale@gmail.com>
2015-08-17 09:57:54 -0600

    torus4 a different perspective

--- hsb_torus4.rb
+++ hsb_torus4.rb
@@ -1,53 +1,42 @@
-# just the inner part of a torus.
+# sprinkle over torus
+# random walk the radius R
+# save as slow frames and edit out to make faster movies.
 
 # use pixels: length to resolve, matrix transformations
 # Load3DImg ?
-require 'matrix'
 
+require 'matrix'
 	def setup
-		frame_rate 8
-		size(600,600)
-		@w,@h = [300] * 2
+		frame_rate 7
+		size(800,800)
+		@w,@h = [400] * 2
 		@i, @j = [0] * 2
 
 		colorMode(HSB,360,100,100)
-		@all_coords = (0..8500).map{ sprinkle }
-		@all_diagonals = (0..900).map{ sprinkle_diag }
+		@all_coords = (0..10000).map{ sprinkle }
+		@all_diagonals = (0..700).map{ sprinkle_diag }
 
 	  text_font create_font("SanSerif",10)
 	end
 
-	# BODY:
-	def set_body_color(x,y,z,matrix)
+	# color setting:
+	def set_color(x,y,z,matrix)
 		a, b, c = matrix.to_a.flatten.map{|x| x/(2* PI.to_f)}
 		color = [(c * 1200) + 200, ((a+10) * 20), 100]
 		# color = [c < 0 ? 100 : 200, a < 0 ? 40 : 90 , 100]
+		# color = [220,70,100]
+
 		set(x+@w, z+@h, color(*color))
 	end
 
-	def abs(i) ; ((i**2)**0.5).to_f ; end
-
-	def sprinkle
-		sin_p, cos_p = sin_cos (rand 360)/360.0
-		sin_t, cos_t = sin_cos (rand 360)/360.0
-
-		# replace cos_p with -abs(cos_p) for just singularity
-		x = (RAD + -abs(cos_p)) * cos_t
-		y = (RAD + -abs(cos_p)) * sin_t
-		z = sin_p
-
-		Matrix.columns([[x,y,z]])
-	end
-	#
-	
-	# DIAGONAL:
 	def set_diag_color(x,y,z,matrix)
 		a, b, c = matrix.to_a.flatten.map{|x| x/(2* PI.to_f)}
 		set(x+@w, z+@h, color(0,0,100))
 	end
+	#
 
 	def sprinkle_diag
-		sin_p, cos_p = sin_cos (rand 360)/360.0 
+		sin_p, cos_p = sin_cos (360 - (rand 360))/360.0 
 
 		x = (RAD + cos_p) * cos_p
 		y = (RAD + cos_p) * sin_p
@@ -55,26 +44,36 @@ require 'matrix'
 
 		Matrix.columns([[x,y,z]])
 	end
-	#
 
-	# one can see that the whole inner core becomes a sphere.
+	def sprinkle
+		sin_p, cos_p = sin_cos (360 - (rand 360))/360.0 
+		sin_t, cos_t = sin_cos (360 - (rand 360))/360.0 
+
+		x = (RAD + cos_p) * cos_t
+		y = (RAD + cos_p) * sin_t
+		z = sin_p
+
+		Matrix.columns([[x,y,z]])
+	end
+
 	# RAD = 0 for circle, RAD 1 for apple, RAD = 2 for torus
-	RAD = 2 ; SCALE = (200 / (1 + RAD).to_f).freeze
+	RAD = 2 ; SCALE = (300 / (1 + RAD).to_f).freeze
 	def sin_cos(var) ; %w(sin cos).map {|s| Math.send(s, 2 * PI * var) } ; end
-	
+
 	def draw
 		clear
 
-		# switch between views
 		cos,sin = %w(cos sin).map{|s| eval("Math.#{s} #{(@i += 0.002)*PI}")}
-		tranny = key == '1' ? [[0,1,0],[cos,sin,0],[cos,0,sin]] :
-													[[1,0,0],[cos,sin,0],[0,cos,sin]]
+		rotation_x = Matrix.rows([[1,0,0],[0,cos,sin],[0,sin,cos]])
 
-		rotation = Matrix.rows(tranny)
+		cos, sin = %w(cos sin).map{|s| eval("Math.#{s} #{0.5*PI}")}
+		rotation_z = Matrix.rows([[cos,sin,0],[sin,cos,0],[0,0,1]])
+
+		rotation = rotation_x * rotation_z
 
 		@all_coords.each do |mtrx|
 			x_y_z = (rotation * SCALE * mtrx).to_a.flatten
-			set_body_color(*x_y_z,mtrx)
+			set_color(*x_y_z,mtrx)
 		end
 
 		@all_diagonals.each do |mtrx|
@@ -82,7 +81,3 @@ require 'matrix'
 			set_diag_color(*x_y_z,mtrx)
 		end
 	end
-
-
-
-
