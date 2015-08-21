184b2f2d64cc2dd17c29bea63bf0915f6b05459a
Jon Zingale <jon@jackrabbitsystems.com>
2015-08-21 11:02:44 -0600

    now with bubbles class

--- falling_circles_of_cyan.rb
+++ falling_circles_of_cyan.rb
@@ -1,12 +1,32 @@
-# The goal here is to make falling
-# cyan circles
+# The goal here is to make falling cyan circles
+
+	class Bubbles
+		def initialize(num)
+	  	@coords = (0..num).map { [rand(2000), rand(2000), 0, 0,
+																170 + rand(90),# hue
+																rand(20) + 70, # sat
+																rand(10) + 90, # bright
+																100            # opacity
+																] }
+	  end
+
+		def trip_x(t) ; rand(20) == 1 ? (-1) ** rand(2) : t ; end
+		def trip_y(s) ; rand(3) == 1 ? rand(2) : s ; end
+
+	  def walk
+	  	@coords.map! do |x, y, s, t, hue, sat, bri, opac|
+	  		coords = [x + trip_x(t) * 3, y + trip_y(s) * 2, s, t]
+	  		coords.map{|i| i % 1920} + [hue,sat,bri,opac]
+	  	end
+	  end
+	end
 
 	def setup
-		grid = 860 ; size(grid,grid)
-		@w, @h = [grid/2.0, 0]
+		size(1920,1000) # JackRabbit
+		@w, @h = [width/2.0, 0]
 		@i = 0 ; @t = 0
 
-    frame_rate 30
+    frame_rate 20
 
 		colorMode(HSB,360,100,100,100)
 	  text_font create_font("SanSerif",10)
@@ -15,32 +35,13 @@
 	  stroke(200,100,100)
 	  stroke_weight(5)
 
-	  @coords = (0..30).map{ [rand(2000),
-	  												rand(2000),
-	  												0,
-	  												0,
-	  												180 + rand(60),# hue
-	  												rand(10) + 90, # sat
-	  												# 100,
-	  												rand(10) + 90, # bright
-	  												# 100
-	  												rand(15) + 85   # opacity
-	  												] }
+	  @coords = Bubbles.new(60)
 	end
-
-	def trip_x(t) ; rand(20) == 1 ? (-1) ** rand(2) : t ; end
-	def trip_y(s) ; rand(3) == 1 ? rand(2) : s ; end
 
 	def draw
 		clear
 
-		@coords.map! do |x, y, s, t, hue, sat, bri, opac|
-			x, y, s, t = [x + trip_x(t) * 3, 
-										y + trip_y(s) * 2, 
-										s, 
-										t
-										].map{|i| i % width}
-
+		@coords.walk.each do |x, y, s, t, hue, sat, bri, opac|
 			stroke(hue,sat,bri,opac)
 			ellipse(x,y, 100, 100)
 			[x,y,s,t,hue,sat,bri,opac]
