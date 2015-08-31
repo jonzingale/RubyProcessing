055d900ca412efd9820beb25b5a2d3a19259fa05
Jon Zingale <jon@jackrabbitsystems.com>
2015-08-31 17:25:02 -0600

    cleaner heat_map with objects and testing ground

--- heat_map_usa2.rb
+++ heat_map_usa2.rb
@@ -3,54 +3,42 @@
 # as the day begins and 'cool down' as the sun sets.
 
 # diff in humidity as bent line?
-
-require 'nokogiri'
-require 'open-uri'
-require 'mechanize'
-
 	DateNow = DateTime.now.strftime('%B %d, %Y').freeze
-	STARTTIME = Time.now.strftime('%l:%M %P').freeze
+	StartTime = Time.now.strftime('%l:%M %P').freeze
 
 	CURRENT_TEMP_SEL = './/p[@class="myforecast-current-lrg"]'.freeze
 	USA_MAP = "/Users/Jon/Desktop/us_maps/us_topographic.jpg".freeze # 1152 × 718
 	USA_MAP_TEMP = '/Users/Jon/Desktop/us_maps/us_topographic_tmp.jpg'.freeze
 	SECONDS = 800.freeze
 
-	# It would be cool to geocode and place somehow.
-	# somekind of linear transformation I suspect.
-
 	BASE_URL = 'http://forecast.weather.gov/MapClick.php?'.freeze
 	CITY_DATA = [['santa fe','87505', [441, 372]],
 							 ['bullhead city','86429', [302, 374]],
 							 ['cleveland','44107', [1041, 251]],
-							 ['monroe','98272', [355, 130]]#,
-							 # ['quakertown','18951', [1147, 230]],
-							 # ['new orleans','70112',[956,571]],
-							 # ['austin','78705',[700,554]],
-							 # ['bad lands','57750',[617,224]],
-							 # ['albuquerque','87101',[420,407]],
-							 # ['san francisco','94101',[197,279]],
-							 # ['bismarck','58501',[706,190]],
-							 # ['helena','59601',[509,223]],
-							 # ['everglades','34139',[1347,707]],
-							 # ['annapolis','21401',[1182,301]],
-							 # ['detroit','48201',[1000,253]],
-							 # ['phoenix','85001',[327,420]],
-							 # ['atlanta','30301',[1065,435]]
+							 ['monroe','98272', [355, 130]],
+							 ['quakertown','18951', [1147, 230]],
+							 ['new orleans','70112',[956,571]],
+							 ['austin','78705',[700,554]],
+							 ['bad lands','57750',[617,224]],
+							 ['albuquerque','87101',[420,407]],
+							 ['san francisco','94101',[197,279]],
+							 ['bismarck','58501',[706,190]],
+							 ['helena','59601',[509,223]],
+							 ['everglades','34139',[1347,707]],
+							 ['annapolis','21401',[1182,301]],
+							 ['detroit','48201',[1000,253]],
+							 ['phoenix','85001',[327,420]],
+							 ['atlanta','30301',[1065,435]]
 							]
 
 	class Place
 		require 'mechanize'
-		require 'nokogiri'
-		require 'open-uri'
-
-		attr_reader :temp, :humidity, :agent, :page
+		attr_accessor :temp, :humidity, :agent, :page, :zipcode, :coords
 
 		def initialize(city,zipcode,coords)
 			@city, @zipcode, @coords = city, zipcode, coords
+			@page = Mechanize.new.get('http://www.weather.gov')
 			@temp, @humidity = 0, 0
-			@agent = Mechanize.new
-			@page = agent.get('http://www.weather.gov')
 		end
 
 		def scrape_temp
@@ -58,11 +46,11 @@ require 'mechanize'
 			form.inputstring = self.zipcode
 			page = form.submit
 
-			self.temp = page.at(CURRENT_TEMP_SEL).text.to_i
+			@temp = page.at(CURRENT_TEMP_SEL).text.to_i
 		end
 	end
 
-#########
+	attr_reader :cities, :loaded
 	def setup
 		text_font create_font("SanSerif",17)
 		square = [1450, 800, P3D] ; size(*square)
@@ -70,84 +58,74 @@ require 'mechanize'
 		frame_rate 1 ; colorMode(HSB,360,100,100)
 		no_stroke
 
-		@my, @mx = [0,0]
 		@i, @t = [0 , 1]
 
 		# border color scale
 		(0..200).each{|i| fill(scale(i-10),100,100)
 											ellipse(i*9,height,20,20) }
 
+		# It would be cool to geocode and place somehow.
+		# somekind of linear transformation I suspect.
 		# scaling is a bitch, don't touch
 		rs = 0.70 ; rotateX(PI/5.0)
 		@loaded = loadImage(USA_MAP)
 		@loaded.resize(1152*rs,718*rs)
 		image(@loaded,350,180)
 
-		scrape_temps
+		@cities = CITY_DATA.map{|data| Place.new(*data) }
+		@cities.each{|city| city.scrape_temp}
 	end
 
 	def counter ; @i = (@i + 1) % SECONDS ; end
-
-	def scrape_temps
-		agent = Mechanize.new ; @data = []
-		page = agent.get('http://www.weather.gov')
-
-		CITY_DATA.each do |city, zip, coords|
-			form = page.form('getForecast')
-			form.inputstring = zip
-			page = form.submit
-
-			temp = page.at(CURRENT_TEMP_SEL).text.to_i
-			@data << [temp,coords]
-		end
-	end
 
 	def scale(temp) # linear
 		scale = 360 * ((136-temp)/136.to_f)
 		translate = scale - 82 % 360
 	end	
 
-	def images
-		# text rendering
-		fill(0,0,0,100) ; rect(100,607,160,50)
+	def map_key
+		fill(0,0,0,90) ; rect(100,570,160,130)
 
-		fill(0,100,100,100)
-		text("started at #{STARTTIME}",100,620)
+		fill(0,100,100)
+		text("started at #{StartTime}",100,620)
 
 		current_time = Time.now.strftime('%l:%M %P')
 		message = "currently #{current_time}"
 		text(message,100,645)
 
-		fill(0,0,100,100)
+		message = "#{(SECONDS/60.0).round(1)} minutes"
+		fill(0,0,100)
+		text(message,140, 678)
 		text(DateNow,100, 590)
 
-		message = "#{(SECONDS/60.0).round(1)} minutes"
-		fill(0,0,100) ; text(message,140, 678)
 		fill(30,100,100) ; rect(100,665,15,15)
+	end
 
-		if @i == 0
-			scrape_temps ; @t += 1
+	def images
+
+		if @i == 0 ; @t += 1
+			cities.each{|city| city.scrape_temp }
+
+			# saves, loads, then displays loaded pic.
 			save(USA_MAP_TEMP)
-
-			# loads, then displays loaded pic.
-			@loaded = loadImage(USA_MAP_TEMP)
+			loaded = loadImage(USA_MAP_TEMP)
 			image(@loaded,0,0)
 		end
 	end
 
 	def draw
 		counter
+		map_key
 
-		@data.each do |temp,coords|
-			hue = scale(temp)
-
-			# add some random walk sway. Make a shadow?
-			# humidity for curve?
-			x, y = coords ; coords = [x , y-@t*7]
-			fill(hue,100,100,70) ; rect(*coords,7,7)
+		# add some random walk sway. 
+		# Make a shadow? humidity for curve?
+		cities.each do |city|
+			hue = scale(city.temp)
+			x, y = city.coords ; coords = [x, y-@t*11]
+			fill(hue,100,100,70) ; rect(*coords,11,11)
 
 			# dont let the number get blurry.
-			# fill(0) ; text("#{temp}",*coords)
+			# fill(200,30,100) ; text("#{city.temp}",*coords)
 		end
 
 		images
