20871534efd6065cb42f06c614b096e9b8702e66
Jonathan Zingale <jonzingale@gmail.com>
2015-10-01 12:21:21 -0600

    steps toward scalability

--- blinky/blinky_lights.rb
+++ blinky/blinky_lights.rb
@@ -4,17 +4,27 @@
 	require (File.expand_path('blinky', File.dirname(__FILE__)))
 
 		def setup
-			size(800,800) ; background(0)
-			frame_rate 3 # what ratio is there for frame_rate, board_size and e_ball?
-			@e_ball = 5 # size of balls
-			@blinky = Blinky.new(150,150) # boardsize (150 for size 800)
+			size(800,800)
+			background 0
+
+			bs = [@width,@height].map{|s| s * 3 / 16}
+
+			frame_rate_hash = {1200 => 1, 1000 => 2, 800 => 3}
+			
+			fr = frame_rate_hash[@height]
+			# fr = (@height/100) *(1/Math.log(15)) # can i scale better?
+
+			@blinky = Blinky.new(*bs)
+
+			frame_rate fr
+			@ball_size = 5
 		end
 
 		def pretty_print(board)
 			
 			board.each_with_index do |row,c_dex|
 				row.each_with_index do |c,r_dex|
-					params = [r_dex,c_dex].map{|i|i*@e_ball+20} + [@e_ball]*2
+					params = [r_dex,c_dex].map{|i|i*@ball_size+20} + [@ball_size]*2
 					rgb = (1..3).map{|i| c*(rand 255)}
 					fill(*rgb) ; ellipse(*params)
 				end
