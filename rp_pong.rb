4fd2955f7d5154ee8aa094990c804cc610fbf296
Jon Zingale <jon@jackrabbitsystems.com>
2015-12-16 22:54:05 -0700

    pong with hypercube

--- rp_pong.rb
+++ rp_pong.rb
@@ -1,4 +1,18 @@
 # RP_Pong
+	class HyperCube
+		require 'matrix'
+		BASES = (0...4).map{|i|2**i}.freeze # 1,2,4,8
+		ALL_POINTS = (0...2**4).inject([],:<<).freeze
+
+		attr_reader :coords, :rand_c
+		def initialize(width, height)
+			@rand_c = [width, height].map{|i|i/3.0}+[0,(0..2).map{rand(255)}]
+			triangles = ALL_POINTS.inject([]){ |a,i| a+=(0...i).map{ |j| [i,j] } }
+			edges = triangles.map{|a,b|[a,b] if BASES.include?(a^b)}.compact
+			@coords = edges.map{|a|a.map{|x|("%04d" % x.to_s(2)).split('').map(&:to_i)}}
+		end
+	end
+
 	class Paddle
 		attr_reader :y_val
 		def initialize
@@ -56,26 +70,20 @@
 
 	def setup
 		text_font create_font("SanSerif",17)
-		square = [1450, 800] ; size(*square)
-		@w,@h = [square[0]/2] * 2 ; background(0)
+		square = [900, 900] ; size(*square)
+		@w, @h = [square[0]/2] * 2 ; background(0)
 		colorMode(HSB,360,100,100)
-		frame_rate 48
-
-		stroke(200,100,100)
+		frame_rate 48 ; @i = 0
 		@ball, @paddle = Ball.new, Paddle.new
+		@cube = HyperCube.new(width, height)
 	end
 
-	def draw_boundary
-		no_fill ; rect(100,100,600,600)
-	end
+	def mouseMoved ; @paddle.move(mouseY) ; end
+	def draw_boundary ; no_fill ; stroke(200,100,100) ; rect(100,100,600,600) ; end
 
-	def draw_ball(coords) # make hypercube?
+	def draw_ball(coords)
 		fill(260, 100, 100)
 		ellipse(*coords, Ball_Size, Ball_Size)
-	end
-
-	def mouseMoved
-		@paddle.move(mouseY)
 	end
 
 	def draw_paddle1(y_val)
@@ -90,6 +98,18 @@
 		ellipse(700, y_val, 20, 200 )
 	end
 
+	def draw_hypercube
+		cos,sin = %w(cos sin).map{|s| eval("Math.#{s} #{(@i+=0.001)*2*PI}")}
+		rotation = Matrix.rows([[cos**2,cos*sin,-sin,0],[-cos*sin,cos**2,0,-sin]])
+
+		transform_edges = [0,1].map do |i|
+			(rotation*Matrix.columns(@cube.coords.transpose[i])* height/12.0).transpose.to_a
+		end.transpose
+
+		transform_edges.each{|edge|a,b = edge.map{|d,e| [d+@w,e+@h]} ; line(*a,*b)}
+		# colors = @cube.rand_c.last
+		stroke(260, 100, 100)
+	end
 
 	def draw
 		clear
@@ -97,5 +117,6 @@
 		draw_boundary
 		draw_paddle1(@paddle.y_val)
 		draw_paddle2(300) # opponent
+		draw_hypercube
 		draw_ball(@ball.coords)
 	end
