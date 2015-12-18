35ea26f3bfae0054df8868ed766f15e5c0b6ef36
Jonathan Zingale <jonzingale@gmail.com>
2015-12-17 18:51:18 -0700

    better pong

--- rp_pong.rb
+++ rp_pong.rb
@@ -2,7 +2,7 @@
 	class HyperCube
 		require 'matrix'
 		BASES = (0...4).map{|i|2**i}.freeze # 1,2,4,8
-		ALL_POINTS = (0...2**4).inject([],:<<).freeze
+		ALL_POINTS = (0...2**4).inject([], :<<).freeze
 
 		attr_reader :coords, :rand_c
 		def initialize(width, height)
@@ -14,13 +14,17 @@
 	end
 
 	class Paddle
-		attr_reader :y_val
-		def initialize
-			@y_val = 200
+		attr_reader :y_val, :vect, :side
+		# TODO:
+		# work on deflecting correctly.
+		# paddle ball collisions.
+		def initialize(side = 'left')
+			@y_val, @vect, @side = 400, 1, side
 		end
 
 		def move(y_val)
-			(@y_val = y_val) if y_val < 600 && y_val > 200
+			boundary_cond = y_val < 600 && y_val > 200
+			@y_val = y_val if boundary_cond
 		end
 	end
 	
@@ -30,37 +34,32 @@
 
 		def initialize
 			@color = [rand(360), 100, 100]
-			@coords = [200,200]
-			theta = theta_init
- 			@vect = [Math.sin(theta), Math.cos(theta)]
-			@inc = 4 # move by increment
-		end
-
-		def theta_init
-			(it = rand) > 0.7 || it < 0.3 ? theta_init : it * TU
+			@coords = 380, 400
+ 			@vect = [Math.sin(TU * rand), 1]
+			@inc = 8 # move by increment
 		end
 
 		def move
-			@coords = @coords.zip(@vect).map{|c,v| c+v*@inc}
-			update_vect
-		end
-
-		def update_vect
-			@vect = boundary?(@coords) ? @vect.zip([1,-1]).map{|v,s|v*s} :
-							paddle?(@coords) ? @vect.zip([-1,1]).map{|v,s|v*s} : 
-							@vect
+			@coords = @coords.zip(@vect).map{|c,v| c + v * @inc}
+			boundary?(@coords)
 		end
 
 		def boundary?(coords)
-			top = coords[1] < Low_Bound
-			bottom = coords[1] > Hi_Bound
-			top || bottom
+			cond = coords[1] < Low_Bound || coords[1] > Hi_Bound
+			(@vect = @vect.zip([1,-1]).map{|v,s|v*s}) if cond
 		end
 
-		def paddle?(coords)
-			left = coords[0] < Low_Bound
-			right = coords[0] > Hi_Bound
-			left || right
+		def abs(val) ; Math.sqrt(val**2) ; end
+
+		def paddle?(coords, paddle)
+			# scoring must happen here somewhere.
+			# drop out condition, bound then check for collision
+			# how should the bounce off happen?
+			left_cond = paddle.side == 'left'
+			cond_1 = left_cond ? coords[0] < Low_Bound : coords[0] > Hi_Bound
+			cond_2 = abs(coords[1] - paddle.y_val) <  100
+
+			@vect = @vect.zip([-1,1]).map{|v,s|v*s} if cond_1 && cond_2
 		end
 	end
 
@@ -69,16 +68,18 @@
 	Hi_Bound = 700 - Ball_Size/2.0
 
 	def setup
-		text_font create_font("SanSerif",17)
-		square = [900, 900] ; size(*square)
-		@w, @h = [square[0]/2] * 2 ; background(0)
+		text_font create_font("SanSerif",40)
+		size(900, 900) ; background(0)
 		colorMode(HSB,360,100,100)
 		frame_rate 48 ; @i = 0
-		@ball, @paddle = Ball.new, Paddle.new
+		@l_score, @r_score = [0] * 2
+		@paddle1 = Paddle.new('left')
+		@paddle2 = Paddle.new('right')
+		@ball = Ball.new
 		@cube = HyperCube.new(width, height)
 	end
 
-	def mouseMoved ; @paddle.move(mouseY) ; end
+	def mouseMoved ; @paddle1.move(mouseY) ; end
 	def draw_boundary ; no_fill ; stroke(200,100,100) ; rect(100,100,600,600) ; end
 
 	def draw_ball(coords)
@@ -88,12 +89,16 @@
 
 	def draw_paddle1(y_val)
 		fill(160, 100, 100)
-		y_val = @ball.coords[1] # delete when no longer funny
 		ellipse(100, y_val, 20, 200 )
 	end
 
+	# slower than ball
+	# change paddle direction
+	# vect roots unity.
+	# std badness.
 	def draw_paddle2(y_val)
 		y_val = @ball.coords[1]
+		@paddle2.move(y_val)
 		fill(160, 100, 100)
 		ellipse(700, y_val, 20, 200 )
 	end
@@ -106,17 +111,34 @@
 			(rotation*Matrix.columns(@cube.coords.transpose[i])* height/12.0).transpose.to_a
 		end.transpose
 
-		transform_edges.each{|edge|a,b = edge.map{|d,e| [d+@w,e+@h]} ; line(*a,*b)}
-		# colors = @cube.rand_c.last
 		stroke(260, 100, 100)
+		transform_edges.each do |edge|
+			a,b = edge.map{|d,e| [d+400,e+400]}
+			line(*a,*b)
+		end
+	end
+
+	def reset
+		text("Left: #{@l_score}", 150, 800)
+		text("Right: #{@r_score}", 450, 800)
+		if @ball.coords[0] < 10 || @ball.coords[0] > 800
+			(@r_score +=1) if @ball.coords[0] < 100
+			(@l_score +=1) if @ball.coords[0] > 700
+			@ball = Ball.new
+		end
 	end
 
 	def draw
-		clear
-		@ball.move
-		draw_boundary
-		draw_paddle1(@paddle.y_val)
-		draw_paddle2(300) # opponent
+		clear ; draw_boundary
+
+		# do i want these here?
+		@ball.paddle?(@ball.coords, @paddle1)
+		@ball.paddle?(@ball.coords, @paddle2)
+
+		draw_paddle1(@paddle1.y_val)
+		draw_paddle2(@paddle2.y_val) # opponent
+
 		draw_hypercube
-		draw_ball(@ball.coords)
+		@ball.move ; draw_ball(@ball.coords)
+		reset
 	end
