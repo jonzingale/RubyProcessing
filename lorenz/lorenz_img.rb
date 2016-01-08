37022c9ca146c0ca2ce3e3be8a6369bf9ac3d3a6
Jon Zingale <jon@jackrabbitsystems.com>
2016-01-08 13:19:26 -0700

    generalized lorenz to n-many trajectories

--- lorenz/lorenz_img.rb
+++ lorenz/lorenz_img.rb
@@ -1,4 +1,4 @@
-# require (File.expand_path('./lorenz', File.dirname(__FILE__)))
+require (File.expand_path('./lorenz', File.dirname(__FILE__)))
 	# binding = $app
 	def setup
 		size(displayWidth, displayHeight)
@@ -6,23 +6,25 @@
 		colorMode(HSB,360,100,100,60)
 		@w, @h = [width/2.0, height/2.0]
 		@i = 0 ; @t = 0
-		no_stroke
 		background(0)
     frame_rate 100
+		no_stroke
 
-		@attractor = Lorenz.new
-		@bttractor = Lorenz.new
+		@trajectories = get_trajectories(3)
+	end
+
+	def get_trajectories num
+		(0...num).map{ Lorenz.new }
 	end
 
 	def plot_lorenz(attractor)
-		x = attractor.x * 20 + @w
-		y = attractor.y * 20 + @h
+		x = attractor.x * 15 + @w
+		y = attractor.y * 15 + @h
 		c = attractor.color
-		fill(*c) ; ellipse(x,y,10,10)
+		fill(*c) ; ellipse(x,y,6,6)
 		attractor.blink
 	end
 
 	def draw
-		plot_lorenz(@attractor)
-		plot_lorenz(@bttractor)
+		@trajectories.each{|t| plot_lorenz t}
 	end
