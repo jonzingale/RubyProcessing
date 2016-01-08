d7b0b6de524d5bcb86862b3a187b16af75064ad3
Jonathan Zingale <jonzingale@gmail.com>
2016-01-08 15:33:13 -0700

    monster fields start

--- bezier/monster_field.rb
+++ bezier/monster_field.rb
@@ -1,26 +1,35 @@
 require (File.expand_path('./bezier', File.dirname(__FILE__)))
+require (File.expand_path('./lorenz', File.dirname(__FILE__)))
 	RES = 40.0.freeze
 	# binding = $app
-	# perhaps following a lorenz?
+
+	# create a field of these guys, with perspective.
+	# better colors? snow?
 
 	def setup
 		size(displayWidth, displayHeight)
 		colorMode(HSB,360,100,100,60)
-		@w, @h = [width/2.0, 0]
+		@w, @h = [width/2.0, height/2.0]
 		@i = 0 ; @t = 0
-    frame_rate 20
+    frame_rate 50
 		background(0)
 
-		@monster = Monster.new 7, width, height
-		stroke_width(160)
+		@monster = Monster.new 2, width, height
+		@attractor = Lorenz.new
+		# stroke_width(120)
+		stroke_width(200)
+		# stroke_width(15)
 	end
 
-	def mouseMoved
-		coords = [mouseX,mouseY]
-		@monster.beziers.map{|bezier| bezier.coords(coords)}
+	def dynamics
+		@attractor.blink
+		x = @attractor.x * 40 + @w/1.2
+		y = @attractor.y * 20 + @h/1.3
+		@monster.beziers.map{|bezier| bezier.coords([x,y])}
 	end
 
-	def draw ; clear
+	def draw
+		clear ; dynamics
 		(0..RES).each do |q|
 			@monster.beziers.each do |bezier|
 				pt = bezier.plot(q/RES)
@@ -34,10 +43,21 @@ require (File.expand_path('./bezier', File.dirname(__FILE__)))
 	class Monster
 		attr_reader :beziers, :color
 		def initialize num, width, height
-			@beziers = (1...num).map do |i|
-				# points = [[0,height], [width/2,rand(3000)], [i * width/num, height]]
-				points = [[0,height], [width/2,rand(30)], [i * width/num, height]]
+			@beziers = (1..num).map do |i|
+				# points = [[0,height], [width/2,rand(10)], [i * width/3/num, height/2]]
+				points = [[0,height], [width/2,rand(10)], [i * rand(width), height/2]]
+
 				Bezier.new(points)
 			end
 		end
 	end
+
+	# class Monster
+	# 	attr_reader :beziers, :color
+	# 	def initialize num, width, height
+	# 		@beziers = (1..num).map do |i|
+	# 			points = [[0,height], [width/2,rand(10)], [i * width/num, height]]
+	# 			Bezier.new(points)
+	# 		end
+	# 	end
+	# end
