5422edd01d4bec922a70284c50cd1eb370381045
Jon Zingale <jon@jackrabbitsystems.com>
2016-01-10 01:37:56 -0700

    monster in a snowdrift, calvin and hobbes do nasa

---
+++ calvinHobbes/calvin_n_hobbes.rb
@@ -0,0 +1,36 @@
+# !/usr/bin/env ruby
+require 'open-uri'
+require 'mechanize'
+# IMAGES_PATH = File.expand_path('./../..', File.dirname(__FILE__)).freeze
+IMAGES_PATH = File.expand_path('.', File.dirname(__FILE__)).freeze
+HobbesPath = "#{IMAGES_PATH}/hobbes.png".freeze
+StarPath = "#{IMAGES_PATH}/nasa_img.jpg".freeze
+
+BaseUrl = 'http://apod.nasa.gov/apod/'.freeze
+Image_Sel = './/img/parent::a'.freeze
+
+def setup
+	size(displayWidth, displayHeight)
+
+  get_nasa_pic
+	@star_image = loadImage(StarPath)
+	image(@star_image, 0, 0, *rescalar)
+
+	@hobbes_image = loadImage(HobbesPath)
+	image(@hobbes_image, 0, 0, 1920, 1040)
+end
+
+def rescalar
+	width = @star_image.width
+	height = @star_image.height
+	s = 1920/@star_image.width.to_f
+	[width * s, height * s]
+end
+
+def get_nasa_pic
+	agent = Mechanize.new
+	page = agent.get(BaseUrl+'astropix.html')
+	stub = page.at(Image_Sel)['href']
+
+	open(StarPath, 'wb') {|f| f << open(BaseUrl+stub).read }
+end
