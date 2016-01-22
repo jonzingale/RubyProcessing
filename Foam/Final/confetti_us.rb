84a8dee872515ba3597c70d161f3fa35cd101df5
Jonathan Zingale <jonzingale@gmail.com>
2016-01-21 17:31:17 -0700

    better confetti and calvinhobbes modification

--- Foam/Final/confetti_us.rb
+++ Foam/Final/confetti_us.rb
@@ -4,8 +4,8 @@ class Bubbles
 															rand(300)+100,		 		# hue
 															rand(100), 						# sat
 															rand(100), 						# bright
-															75+rand(25),  				# opacity
-															50+rand(200),							# radius_var
+															05+rand(25),  				# opacity
+															50+rand(200),					# radius_var
 															] }
   end
 
@@ -29,12 +29,15 @@ class RainyWindowWithVideo < Processing::App
 	def setup
 		colorMode(HSB,360,100,100,100)
 		size(displayWidth, displayHeight)
-		# size(800,800)
 		@w, @h = [width/2.0, 0]
 		@i = 0 ; @t = 0
 	  frame_rate 80 #80 #60 # can be higher.
 	  text_font create_font("SanSerif",50)
-	  @coords = Bubbles.new(12)
+	  background(100,0,0,0)
+	  ellipse_mode(CORNER)
+		no_stroke
+
+	  @coords = Bubbles.new(32)
 	  @sample_rate = 10
 	  @capture = Capture.new(self, width, height, 30)
 	  @capture.start
@@ -43,28 +46,21 @@ class RainyWindowWithVideo < Processing::App
 	def draw
 		@i = (@i+1)%360
 
-	  if @i % 4 == 0 && capture.available
+	  if @i % 3 == 0 && capture.available
 	  # if capture.available
 		  capture.read
 		  convert_pixels
 	 	end
 
 		@coords.walk.each do |x, y, s, t, hue, sat, bri, opac, r|
-			stroke(hue,sat,bri,Math.sin(@i)*opac)
-			fill(hue,sat,bri,Math.sin(@i)*opac)
+			# stroke(hue,sat,bri,Math.sin(@i)*opac)
+			fill(hue, sat+rand(60), bri, Math.sin(@i)*opac)
 			ellipse(x, y, 12+ r, 12+ r)
 			[x,y,s,t,hue,sat,bri,opac,r]
 		end
 	end
 
-  def clear
-  	background(100,0,0,0)
-    no_stroke
-    ellipse_mode(CORNER)
-  end
-  
   def convert_pixels
-    # clear
     (1...height).step(sample_rate) do |y|
       (1...width).step(sample_rate) do |x|        
         pixel = y * capture.width + x
@@ -73,7 +69,8 @@ class RainyWindowWithVideo < Processing::App
         sat = saturation(capture.pixels[pixel])
         bri = brightness(capture.pixels[pixel])
 
-        e_width = map(saturation(capture.pixels[pixel]), 0, 200, 0, 80)#100)
+        e_width = map(saturation(capture.pixels[pixel]),
+        							0, 200, 0, rand(80) )#100)
 
         fill(hue, sat, bri, 40) #255)
         ellipse(x, y, e_width, e_width)        
