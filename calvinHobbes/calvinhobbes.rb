cb20e7881463e01f2bdf9d17d11233464231d533
Jon Zingale <jon@jackrabbitsystems.com>
2016-01-22 12:51:23 -0700

    better calvin and hobbes

--- calvinHobbes/calvinhobbes.rb
+++ calvinHobbes/calvinhobbes.rb
@@ -29,8 +29,10 @@ def setup
 	calvin_or_hobbes = [HobbesPath, CalvinPath][rand 2]
 	@hobbes_image = loadImage(calvin_or_hobbes)
 
-	image(@hobbes_image, 0, 0, width, height)
-	# image(@hobbes_image, 0, 0, 1920, 1040)
+	# image(@hobbes_image, 0, 0, width, height)
+	image(@hobbes_image, 0, 0, 1920, 1080)
+
+	save("#{IMAGES_PATH}/new_nasa.png")
 end
 
 def rescalar
