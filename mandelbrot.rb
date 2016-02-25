f598257c760e61a944e30203f442b12527d682da
Jon Zingale <jon@jackrabbitsystems.com>
2016-02-25 02:23:37 -0700

    mandelbrot sets

---
+++ mandelbrot.rb
@@ -0,0 +1,42 @@
+	class Mandelbrot
+		Escape = 10**20.freeze
+		Limit = 250.freeze
+
+		attr_reader :point, :iterate, :z
+
+		def initialize
+			@point = Complex(ranged_rand * 1.4 - 0.5, ranged_rand * 1.2)
+			@iterate, @z =  0, 0
+			produce
+		end
+
+		def escape? ; Escape < @z.abs || Limit < @iterate ; end
+		def ranged_rand ; (-1)**rand(2) * rand ; end
+		def produce ; blink until escape? ; end
+
+		def blink(c=1)
+			@z = @z ** 2 + @point
+			@iterate += 1
+		end
+	end
+
+	def setup
+		size(displayWidth, displayHeight)
+		@w, @h = width/2.0 + 200, height/2.0 - 50
+		colorMode(HSB,360,100,100,100)
+		background(0)
+    frame_rate 17
+	end
+
+	def draw
+		1000.times do
+			it = Mandelbrot.new	
+			coords = it.point * 400 + Complex(@w, @h)
+
+			tuned_color = Math.log(it.iterate) * 59 + 10
+			filtered_bright = it.iterate < 15 ? 0 : 100
+			
+			c = color(tuned_color, 100, filtered_bright)
+			set(*coords.rect, c)
+		end
+	end
