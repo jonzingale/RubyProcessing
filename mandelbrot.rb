301c6b8a13b1cd8682d5d94fe3842f1f7a7e8591
Jonathan Zingale <jonzingale@gmail.com>
2016-02-25 10:27:15 -0700

    more mandelbrot

--- mandelbrot.rb
+++ mandelbrot.rb
@@ -2,21 +2,21 @@
 		Escape = 10**20.freeze
 		Limit = 250.freeze
 
-		attr_reader :point, :iterate, :z
+		attr_reader :point, :step, :z
 
 		def initialize
 			@point = Complex(ranged_rand * 1.4 - 0.5, ranged_rand * 1.2)
-			@iterate, @z =  0, 0
+			@step, @z =  0, 0
 			produce
 		end
 
-		def escape? ; Escape < @z.abs || Limit < @iterate ; end
+		def escape? ; Escape < @z.abs || Limit < @step ; end
 		def ranged_rand ; (-1)**rand(2) * rand ; end
 		def produce ; blink until escape? ; end
 
 		def blink(c=1)
 			@z = @z ** 2 + @point
-			@iterate += 1
+			@step += 1
 		end
 	end
 
@@ -33,8 +33,8 @@
 			it = Mandelbrot.new	
 			coords = it.point * 400 + Complex(@w, @h)
 
-			tuned_color = Math.log(it.iterate) * 59 + 10
-			filtered_bright = it.iterate < 15 ? 0 : 100
+			tuned_color = Math.log(it.step) * 59 + 10
+			filtered_bright = it.step < 15 ? 0 : 100
 			
 			c = color(tuned_color, 100, filtered_bright)
 			set(*coords.rect, c)
