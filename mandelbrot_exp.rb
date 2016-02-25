d715336dac5aa07701ba65d103bffae7f72302c2
Jon Zingale <jon@jackrabbitsystems.com>
2016-02-25 10:27:15 -0700

    more mandelbrot

--- mandelbrot_exp.rb
+++ mandelbrot_exp.rb
@@ -4,11 +4,8 @@
 
 		attr_reader :point, :iterate, :z
 
-		def initialize
-			@point = Complex(0,0)
-			@z, @iterate = 0, 0
-			produce
-		end
+		def escape? ; Escape < @z.abs || Limit < @iterate ; end
+		def produce ; blink until escape? ; end
 
 		def set_point(pt)
 			@z, @iterate = 0, 0
@@ -16,47 +13,40 @@
 			produce
 		end
 
-		def escape? ; Escape < @z.abs || Limit < @iterate ; end
-		def produce ; blink until escape? ; end
-
 		def blink(c=1)
 			@z = @z ** 2 + @point
 			@iterate += 1
 		end
 	end
 
-	def setup
-		text_font create_font("SanSerif",100)
+	# Todo:
+	# free from window size for rendering
 
+	def setup
 		size(displayWidth, displayHeight)
-		@w, @h = width/2.0 + 200, height/2.0 - 20
+		@w, @h = width/2.0, height/2.0
 		colorMode(HSB,360,100,100,100)
-		background(0)
-    frame_rate 2
+		background(0); @y = 0
+    frame_rate 10
 
     @it = Mandelbrot.new
-
-    @img = createImage(width, height, RGB)
-		@img.loadPixels()
-		@count = @img.pixels.count
-		image(@img, 0, 0)
+    @unscalar = Complex(width / 7, height / 7)
 	end
 
-	def rescale x, y, step
-		[x * step/1920.0 * 4 - 2, y * step/1080.0 * 2.4 - 1.2]
+	def rescale x, y
+		[x / width.to_f * 4 - 2, y / height.to_f * 2.4 - 1.2 ]
 	end
 
 	def draw
-		step = 15
-		(1...height/step).each do |y|
-			(1...(@w+20)/step).each do |x|
-				resized = rescale(x,y,step)
-				z = Complex(*resized)
-				@it.set_point(z)
-		
-				coords = @it.point * 300 + Complex(@w, @h)
-				c = color(@it.iterate, 100, @it.iterate < 13 ? 0 : 100)
-				set(*coords.rect, c)
-			end
+		@y = @y + 1 % height
+
+		width.times do |x|
+			resized = rescale(x, @y)
+			z = Complex(*resized)
+			@it.set_point(z)
+			
+			c = color(@it.iterate, 100, @it.iterate < 13 ? 0 : 100)
+			coords = @unscalar * @it.point + Complex(@w+100, @h+20)
+			set(*coords.rect, c)
 		end
 	end
