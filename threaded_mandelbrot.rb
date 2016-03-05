be15b57162320e9234a992e0f1e8aa5dc9c473f6
Jon Zingale <jon@jackrabbitsystems.com>
2016-03-04 21:56:12 -0700

    threaded mandlebrot

--- threaded_mandelbrot.rb
+++ threaded_mandelbrot.rb
@@ -2,59 +2,58 @@ class Mandelbrot
 	Escape = 10**30.freeze
 	Limit = 255.freeze
 
-	attr_reader :point, :step, :z, :hsb
-	def escape? ; Escape < @z.abs || Limit < @step ; end
-	def produce ; blink until escape? ; end
-
 	def initialize(width, height)
-		@width, @height = width.to_f, height.to_f
+		@width, @height = 4.2 / width, -2.5 / height
+		@scale = 1 # scaling correctly requires a center.
 	end
 
-	def to_scaled_complex x, y
-		Complex(2 - y / @height * 3, x / @width * 4.2 - 1.2)
+	def produce
+		while @z.abs < Escape && @step < Limit 
+			@z = @z * @z + @point
+			@step += 1
+		end
+	end
+
+	def to_complex x, y
+		Complex(x * @width, y * @height) * Complex(@scale) + Complex(-2.8, 1.2)
 	end
 
 	def get_color(w, h)
-		@point = to_scaled_complex w, h
+		@point = to_complex w, h
 		@z, @step = 0, 0
-		produce
-		set_color
+		produce ; set_color
 	end
 
 	def set_color
-		tuned_hue = Math.log(@step) * 50 + 10
+		tuned_hue = Math.log(@step) * 50 + 15
 		brightness = @step < 15 ? 0 : 100
-		@hsb = [tuned_hue, 100, brightness]
-	end
-
-	def blink
-		@z = @z * @z + @point
-		@step += 1
+		[tuned_hue.to_i, 100, brightness]
 	end
 end
 
 CORES = 8.freeze
 
 def setup
-	size(displayWidth/2, displayHeight/2)
-	colorMode(HSB,360,100,100,100)
-	background(0) ; frame_rate 20
+	size(displayWidth, displayHeight)
+	colorMode(HSB,360,100,100)
+	frame_rate 10
+	background 0
   load_pixels
 
-  @mandelbrot = Mandelbrot.new(width, height)
-  @pairs = [*0..height].product([*0..width])
-
-  @threads = []
-  CORES.times{|i| @threads << Thread.new { calculate i }}
+  CORES.times do |i| 
+  	Thread.new { calculate i }
+  end
 end
 
-def calculate(thread=nil)
-		@pairs.each do |x, y|
-			hsb = @mandelbrot.get_color x, y
-			pixels[x*width+y] = color(*hsb)
-		end
+def calculate t
+	m = Mandelbrot.new(width, height)
 
-	calculate thread
+	while t < width * height
+		x, y = t % width, t / width
+		hsb = m.get_color(x, y)
+		pixels[t] = color(*hsb)
+		t += CORES
+	end
 end
 
 def draw
