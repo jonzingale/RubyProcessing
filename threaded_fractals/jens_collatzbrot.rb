12ce24759771dfecaa7ed682261fed4376b90016
Jon Zingale <jon@jackrabbitsystems.com>
2016-04-19 20:50:08 -0600

    towards a threaded_collatz_fractal

--- threaded_fractals/jens_collatzbrot.rb
+++ threaded_fractals/jens_collatzbrot.rb
@@ -1,40 +1,37 @@
-class Mandelbrot
-	Escape = 1000.freeze
-	# Limit = 250.freeze
-	Limit = 30
-	PI = 3.1415926
+class JensCollatz
+	Escape = 10**10.freeze
+	Limit = 255.freeze
 
 	attr_reader :point, :iterate, :z
-	# def escape? ; Escape < @z.abs || Limit < @iterate ; end
 	def escape? ; Escape < @z.abs || Limit < @iterate ; end
-
 	def produce ; blink until escape? ; end
 
 	def set_point(pt)
-		@z, @iterate, @point = 100, 0, pt
+		@z, @iterate, @point = 0, 0, pt
 		produce
 	end
 
 	def blink(c=1)
 		# @z = @z ** 2 + @point
-		@z = jens + @point
+		@z = function + @point
 		@iterate += 1
 	end
 
-	def jens
-		trig = Complex(Math.cos(PI*@z.real),Math.cos(PI*@z.imag))
+	def function
+		trig = Complex(Math.cos(PI*@z.real),
+									 Math.cos(PI*@z.imag))
 		0.25*(1+4*@z-(1+2*@z)*trig)
 	end
 end
 
 def setup
-	size(displayWidth/2.0, displayHeight/2.0)
+	size(displayWidth, displayHeight)
 	@w, @h = width/2.0, height/2.0
 	colorMode(HSB,360,100,100,100)
 	background(0); @y = 0
-  frame_rate 10
+  frame_rate 1
 
-  @it = Mandelbrot.new
+  @it = JensCollatz.new
   @unscalar = Complex(width / 7, height / 7)
 end
 
@@ -50,8 +47,7 @@ def draw
 		z = Complex(*resized)
 		@it.set_point(z)
 
-		c = color(@it.iterate, 100, @it.iterate < 0 ? 0 : 100)		
-		# c = color(@it.iterate, 100, @it.iterate < 13 ? 0 : 100)
+		c = color(@it.iterate, 100, @it.iterate < 0 ? 0 : 100)	# < valid colors
 		coords = @unscalar * @it.point + Complex(@w+100, @h+20)
 		x, y = coords.rect.map(&:to_i)
 		set(*coords.rect, c)
