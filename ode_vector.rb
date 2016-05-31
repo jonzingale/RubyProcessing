2b221979a3f7f0a0085b50440b11e298a924e102
Jon Zingale <jon@jackrabbitsystems.com>
2016-05-31 09:29:20 -0600

    nonlinear settings

--- ode_vector.rb
+++ ode_vector.rb
@@ -2,21 +2,34 @@
 		size(displayWidth, displayHeight)
 		colorMode(HSB,360,100,100,100)
 		@w, @h = [width/2.0, height/2.0]
-    frame_rate 10
+    frame_rate 20
 
 		background(0)
-		stroke(200,100,100,100)
-		stroke_width 1
-		@pts = points 1700
-		@del_t = 0.01
+		stroke(200,0,100,30)
+		stroke_width 0.1
+		@pts = points 12000
+		@del_t = 0.009
+		# @del_t = 1
 	end
 
 	def points num
-		(1..num).map {[rand(width)-@w ,rand(height)-@h]}
+		(1..num).map do
+			# [(rand(width)-@w)/10.0 ,(rand(height)-@h)/400.0]
+			[(rand(width)-@w)/5.0 ,(rand(height)-@h)/2.0]
+
+		end
 	end
 
 	def diff(x,y)
-		[-y,-x+y]
+		# [-y,-x+y]
+		# [-y,x]
+		# [y-x, -x]
+		# [x % 2 < 1 ? x*1.3 : -y, x]
+		[x % 2 < 0.5 ? x+y*3 : -y, x - y]
+		# [x % 2 < 0.5 ? x+1+y*3 : 2-y, y % 3 < 0.5 ? 3*(y+1.5) : - x]
+		# it = [y % 3 < 0.5 ? 3*(y+1.5) : - x, x % 2 < 0.5 ? x+2+y*3 : 2-y]
+		# it[1] % 5 < 4.8 ? it.reverse : it
+ 		# [x/2, 3]
 	end
 
 	def euler
@@ -46,8 +59,11 @@
 		# clear
 		improved_euler
 		@pts.zip(@next_pts).each do |(x,y),(s,t)|
-			stroke rand(360), 30, 100, 50
-			line x+@w, y+@h, s+@w, t+@h
+			# stroke rand(360), 70, 100, 50
+
+			# line x+@w, y+@h, s+@w, t+@h
+			line (x+@w)/1.0, (y+@h)/1.0, (s+@w)/1.0, (t+@h)/1.0
+
 		end
 
 		@pts = @next_pts.map do |x,y|
