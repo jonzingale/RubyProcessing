7be9487609e477b67662845129e33dee216ed13d
Jon Zingale <jon@jackrabbitsystems.com>
2016-06-06 12:12:11 -0600

    better volterra-lotka

--- some_models.rb
+++ some_models.rb
@@ -5,41 +5,40 @@
     frame_rate 15
 
 		background(0)
-		stroke(210,100,100,10)
+		stroke(210,100,100,30)
 		stroke_width 1
-		@pts = points 12000
-		@del_t = 0.03
+		@pts = points 10000
+		@del_t = 0.0007
 	end
 
 	PI = 3.1415926
 	def points num
 		(1..num).map do
 
-			[6 * ((rand * 2 * PI)- PI),
-			 3 * rand * PI, # HALF
-			 # 2 * ((rand * 2 * PI)- PI), # FULL
-			 rand(300)]
+			[2 * ((rand * 2 * PI)- PI),
+			 # 3 * rand * PI, # HALF
+			 2 * ((rand * 2 * PI)- PI), # FULL
+			 rand(100)]
 		end
 	end
 
 	def abs(n) ; (n**2)**0.5 ; end
 
 	def diff(x,y,z)
-		# nonlinear oscillator
-		# b = 1 ; [	y, -b*y - Math.sin(x), z]
-
-		# a,b,c,d = 1,2,1,1
-		# x = a*x-b*x*y
-		# y = -c*y+d*x*y
+		# volterra-lotka
+		# a,b,c,d = [2/3.0,4/3.0,1,1]
+		# a,b,c,d = [0.1,1,1,1]
+		# x = a*x - b*x*y
+		# y = -c*y + d*x*y
 
 		# harvested volterra-lotka
-		# a, b, h = 1, 1, 0
-		# x = x*(1-a*x-y)
-		# y = y*(b-x-y+h)
+		a, b, h = [0.1] * 3
+		x = x*(1-a*x-y)
+		y = y*(b-x-y)+h
 
 		# van der pol
-		x = y - x**3 + x
-		y = -x
+		# x = y - x**3 + x
+		# y = -x
 
 		# x = Math.sin(x)*(-0.1*Math.cos(x)-Math.cos(y))
 		# y = Math.sin(y)*(Math.cos(x)-0.1*Math.cos(y))
@@ -74,17 +73,16 @@
 
 	MU = 100
 	def draw
+		# fill 0,0,0,1
+		# rect(0,0,width,height)
+
 		# clear
 		improved_euler
 		@pts.zip(@next_pts).each do |(x,y,z),(s,t,r)|
-			# stroke 200+r, 100, 100, 100
-
-			line ((x+@w)*MU-@w*MU+@w+360)/2.5, ((y+@h)*MU-@h*MU/1.01)/1.0,
-					 ((s+@w)*MU-@w*MU+@w+360)/2.5, ((t+@h)*MU-@h*MU/1.01)/1.0
+			# stroke 210, 100+z, 100, 20
+			line ((x+@w)*MU-@w*MU+@w), ((y+@h)*MU-@h*MU/1.01),
+					 ((s+@w)*MU-@w*MU+@w), ((t+@h)*MU-@h*MU/1.01)
 		end
 
-		@pts = @next_pts.map do |x,y,z|
-			[x,y,z]
-			# [rand(width)-@w,rand(height)-@h,z]
-		end
+		@pts = @next_pts
 	end
