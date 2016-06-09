50673797b444eac72077e6a566fe890a154f8d60
Jon Zingale <jon@jackrabbitsystems.com>
2016-06-08 19:12:08 -0600

    on the sun

--- ode/volterra_lotka.rb
+++ ode/volterra_lotka.rb
@@ -6,35 +6,36 @@
 
 		background(0)
 		stroke(210,100,100,100)
-		stroke_width 0.3
-		@pts = points 9000
-		@del_t = 0.007
+		stroke_width 0.9
+		# stroke_width 10
+		@pts = points 3000
+		@del_t = 0.03
+		@cds = 0, 0
 	end
 
 	def points num
 		(1..num).map do
-			[rand * 2, 9 * rand * (-1)**rand(2), rand(100)]
+			[rand, rand, rand(210)]
 		end
 	end
 
-	def abs(n) ; (n**2)**0.5 ; end
-
 	def diff(x,y,z)
 		# volterra-lotka
-		# a,b,c,d = [2/3.0,4/3.0,1,1]
-		# a,b,c,d = [0.1,0.4,1,1]
+		# a,b,c,d = [1, 2, # prey: birth/death
+							 # 0.5, 5] # predator : death/birth
 		# x = a*x - b*x*y
 		# y = d*x*y - c*y
 
 		# limited growth
-		a, b, c, d, lam, mu = [2/3.0, 4/3.0, 2, 10, 5, 5]
-		x = x * (a - b*y - lam * x)
-		y = y * (-c + d*x - mu * y)
+		# a, b, c, d, lam, mu = [1.4, -0.5, -0.5, 2, 4, 1]
+		# x = x * (a - b*y - lam * x)
+		# y = y * (-c + d*x - mu * y)
 
 		# with harvesting
-		# a,b,h = 5, 2, 7
-		# x = x*(1-a*x-y)
-		# y = y*(b-x-y)+h
+		a,b,h = 2, 0.5, 0.1
+		x = x*(1-a*x-y)
+		y = y*(b-x-y)+h
+		z = -0.3*z
 		# z = abs(y*x)
 		[x,y,z]
 	end
@@ -57,17 +58,17 @@
 		end
 	end
 
-	Xu, Yu = 1500, 60
+	Xu, Yu = 1900, 1200
 
 	def draw
 		euler
 		# fill(0,0,0,10) ; rect(0,0,width,height)
 		@pts.zip(@next_pts).each do |(x,y,z),(s,t,r)|
-			stroke z, 100, 100, 80
-			if y**2+t**2 < 150
-				line Xu*x+40, Yu*y+@h, Xu*s+40, Yu*t+@h
-			end
+			stroke z/3, 100, 100, 90
+			line Xu*x, Yu*y, Xu*s, Yu*t
 		end
+
+		# no_fill ; ellipse(10,10,10,10)
 
 		@pts = @next_pts
 	end
