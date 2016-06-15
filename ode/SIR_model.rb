a24b4ea241ffc522d3a240e162752b44d5a20377
Jon Zingale <jon@jackrabbitsystems.com>
2016-06-15 12:03:36 -0600

    SIR models

---
+++ ode/SIR_model.rb
@@ -0,0 +1,64 @@
+# SIR models
+	def setup
+		size(displayWidth, displayHeight)
+		colorMode(HSB,360,100,100,100)
+		stroke(210,100,100,100)
+		@w, @h = width, height
+		stroke_width 1
+    frame_rate 15
+		background(0)
+
+		@pts = points 10000
+		@del_t = 0.003
+	end
+
+	def points num
+		(1..num).map { [rand, rand, rand] }
+	end
+
+	def diff(x,y,z)
+		# SIR
+		b, v, u = 4, 2, 0
+
+		s = -b*x*y + u*z
+		i =  b*x*y - v*y
+		r =  v*y*z - u*z
+
+		# s = -b*x*y + 0*y*z + u*z*x
+		# i =  b*x*y - v*y*z + 0*z*x
+		# r =  0*x*y + v*y*z - u*z*x
+
+		[s,i,r]
+	end
+
+	def euler
+		@next_pts = @pts.map do |x,y,z|
+			s, t, r = diff x, y, z
+			dx = x + s * @del_t
+			dy = y + t * @del_t
+			dz = z + r * @del_t
+
+			s, t, r = diff dx, dy, dz
+			ddx = dx + s * @del_t
+			ddy = dy + t * @del_t
+			ddz = dz + r * @del_t
+
+			[(dx + ddx) /2.0, 
+			 (dy + ddy) /2.0,
+			 (dz + ddz) /2.0]
+		end
+	end
+
+	Xu, Yu = 1900, 1000
+
+	def draw
+		euler
+		# clear
+		@pts.zip(@next_pts).each do |(x,y,z),(s,t,r)|
+			stroke y*100, 100, 100, 10
+			# line Xu*x, Yu*z, Xu*s, Yu*r
+			line Xu*x, Yu*y, Xu*s, Yu*t
+		end
+
+		@pts = @next_pts
+	end
