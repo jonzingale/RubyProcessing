70f7389ca2b4483d772d654daf4148c59574898e
Jon Zingale <jon@jackrabbitsystems.com>
2016-06-14 21:55:58 -0600

    S:

--- ode/some_models.rb
+++ ode/some_models.rb
@@ -2,22 +2,20 @@
 		size(displayWidth, displayHeight)
 		colorMode(HSB,360,100,100,100)
 		@w, @h = [width/2, height/2]
-    frame_rate 15
+    frame_rate 20
 
 		background(0)
 		stroke(210,100,100,30)
 		stroke_width 1
-		@pts = points 10000
+		@pts = points 6000
 		@del_t = 0.0007
 	end
 
 	PI = 3.1415926
 	def points num
 		(1..num).map do
-
 			[2 * ((rand * 2 * PI)- PI),
-			 # 3 * rand * PI, # HALF
-			 2 * ((rand * 2 * PI)- PI), # FULL
+			 2 * ((rand * 2 * PI)- PI),
 			 rand(100)]
 		end
 	end
@@ -25,24 +23,10 @@
 	def abs(n) ; (n**2)**0.5 ; end
 
 	def diff(x,y,z)
-		# volterra-lotka
-		a,b,c,d = [2/3.0,4/3.0,1,1]
-		# a,b,c,d = [0.1,0.4,1,1]
-		x = a*x - b*x*y
-		y = d*x*y - c*y
-
-		# harvested volterra-lotka
-		# a, b, h = [0.1] * 3
-		# x = x*(1-a*x-y)
-		# y = y*(b-x-y)+h
-
 		# van der pol
-		# x = y - x**3 + x
-		# y = -x
+		# [ y - x**3 + x, -x , x]
 
-		# x = Math.sin(x)*(-0.1*Math.cos(x)-Math.cos(y))
-		# y = Math.sin(y)*(Math.cos(x)-0.1*Math.cos(y))
-		[x,y,z]
+		[ y**2 - x**3 * Math.sin(x), -x , 1]
 	end
 
 	def euler
@@ -79,7 +63,7 @@
 		# clear
 		improved_euler
 		@pts.zip(@next_pts).each do |(x,y,z),(s,t,r)|
-			# stroke 210, 100+z, 100, 20
+			stroke z, 100, 100, 20
 			line ((x+@w)*MU-@w*MU+@w), ((y+@h)*MU-@h*MU/1.01),
 					 ((s+@w)*MU-@w*MU+@w), ((t+@h)*MU-@h*MU/1.01)
 		end
