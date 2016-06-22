5d85cb891017843993722982ad19c80d3df639e8
Jon Zingale <jon@jackrabbitsystems.com>
2016-06-22 15:53:31 -0600

    ode vector

--- ode/ode_vector.rb
+++ ode/ode_vector.rb
@@ -4,10 +4,10 @@ require 'matrix'
 		size(displayWidth, displayHeight)
 		colorMode(HSB,360,100,100,100)
 		@w, @h = width/2.0, height/2.0
-    frame_rate 20 ; background 0
-		stroke_width 1
-		@pts = points 12
-		@del_t = 0.2
+    frame_rate 10 ; background 0
+		stroke_width 0.1
+		@pts = points 2200
+		@del_t = 0.07
 	end
 
 	# def trigs(theta)#:: Theta -> R2
@@ -19,7 +19,7 @@ require 'matrix'
 	# end
 
 	def cent_rand
-		10 * (rand - 1 * rand)
+		100 * (rand - 1 * rand)
 	end
 
 	def points num
@@ -30,9 +30,9 @@ require 'matrix'
 
 	def diff(vect)
 		m = Matrix.columns([
-			[0,-1 ,0],
-			[1, 0, 0],
-			[0,1,0],
+			[-0.3,1 ,0],
+			[-0.7, 0.3, 0],
+			[-1,0,1],
 		])
 
 		vect * m
@@ -42,34 +42,19 @@ require 'matrix'
 		@next_pts = @pts + diff(@pts) * @del_t
 	end
 
-	# def improved_euler
-		# @next_pts = @pts.map do |v|
+	def improved_euler
+		dm = @pts + diff(@pts) * @del_t
+		ddm = dm + diff(dm) * @del_t
+		@next_pts = (dm + ddm)/2.0
+	end
 
-			# v + diff(v)
-	# 		s, t, r = diff(v).to_a
-	# 		dx = x + s * @del_t
-	# 		dy = y + t * @del_t
-	# 		dz = z + r * @del_t
-
-	# 		s, t, r = diff dx, dy, dz
-	# 		ddx = dx + s * @del_t
-	# 		ddy = dy + t * @del_t
-	# 		ddz = dz + r * @del_t
-
-	# 		[(dx + ddx) /2.0,
-	# 		 (dy + ddy) /2.0,
-	# 		 (dz + ddz) /2.0]
-	# 	end
-	# end
-
-	Xu, Yu = 1, 1
+	Xu, Yu = 10, 10
 
 	def draw
 		# clear
-		# improved_euler
-		euler
+		improved_euler
 		@pts.to_a.zip(@next_pts.to_a).each do |(x,y,z),(s,t,r)|
-			stroke y%360, 100, 100, 70
+			stroke (y*x)%360, 100*y*x, 100, 100
 			# line (Xu*x)+@w, (Yu*z)+@h, (Xu*s)+@w, (Yu*r)+@h
 			line (Xu*x)+@w, (Yu*y)+@h, (Xu*s)+@w, (Yu*t)+@h
 		end
