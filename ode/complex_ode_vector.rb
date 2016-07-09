77489527b1a21734de6eee660a0bebf835a119db
Jon Zingale <jon@jackrabbitsystems.com>
2016-07-09 13:37:47 -0600

    ideas toward working with complex odes

--- ode/complex_ode_vector.rb
+++ ode/complex_ode_vector.rb
@@ -1,38 +1,29 @@
 require 'matrix'
+require 'cmath'
+include CMath
 
 	def setup
 		size(displayWidth, displayHeight)
 		colorMode(HSB,360,100,100,100)
-		@w, @h = width/2.0, height/2.0
-    frame_rate 60 ; background 0
-		stroke_width 2
-		@pts = points 400#00
+		@w, @h = [width/2, height/2]
+    frame_rate 10
+
+		background(0)
+		stroke(210,100,100,100)
+		stroke_width 1
+		@pts = points 9000
 		@del_t = 0.03
-		@t = 1
 	end
 
-	# def trigs(theta)#:: Theta -> R2
-	#   %w(cos sin).map{|s| eval("Math.#{s} #{theta}")}
-	# end
-
-	# def rootsUnity(numbre)#::Int -> [trivalStar]
-	# 	(0...numbre).map{|i|trigs(i*2*PI/numbre)}
-	# end
-
-	def cent_rand
-		100 * (rand - 1 * rand)
-		# -150 * rand
-	end
+	def cent_rand ; 10 * (rand - 1 * rand) ; end
 
 	def points num
-		Matrix.build(num, 3) {|row, col| cent_rand }
+		Matrix.build(num, 3) {|row, col| Complex(cent_rand, cent_rand) }
 	end
-
-	def abs(n) ; (n**2)**0.5 ; end
-
+	
 	def diff(vect)
 		m = Matrix.columns([
-			[-0.3,-1 ,0],
+			[-0.3,-1 ,-1],
 			[0.7, 0.3, 0],
 			[-1,1,0],
 		])
@@ -57,9 +48,10 @@ require 'matrix'
 		improved_euler
 		@pts.to_a.zip(@next_pts.to_a).each do |(x,y,z),(s,t,r)|
 			stroke (x*y*4)%360, (200*x*y)%100, 100, 40
-			# stroke_width(abs(x/y))
+
 			line (Xu*x)+@w, (Yu*z)+@h, (Xu*s)+@w, (Yu*r)+@h
 			# line (Xu*x)+@w, (Yu*y)+@h, (Xu*s)+@w, (Yu*t)+@h
 		end
+		
 		@pts = @next_pts
 	end
