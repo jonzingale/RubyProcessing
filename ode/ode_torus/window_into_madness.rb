aaa9c2408fdc67d5a9f8638c793deb5bc2aadad0
Jon Zingale <jon@jackrabbitsystems.com>
2016-08-10 18:04:49 -0600

    window into madness

---
+++ ode/ode_torus/window_into_madness.rb
@@ -0,0 +1,173 @@
+# Flows on a Torus
+# require 'ode_torus/euler.rb'
+module Torus
+	include Math
+	Tau = 2 * PI
+	RAD = 2 # 0, 1, 2
+	SCALE = 450 / (1 + RAD)
+
+	def sin_cos(var)
+		[:sin, :cos].map {|s| send(s, Tau * var) }
+	end
+
+	def set_key
+		@key = key unless key == '0'
+	end
+
+	def to_torus(x,y)
+		sin_p, cos_p = sin_cos x
+		sin_t, cos_t = sin_cos y
+
+		x = (RAD + cos_p) * cos_t
+		y = (RAD + cos_p) * sin_t
+		z = sin_p
+
+		it = [x,y,z]
+		it = @key == '1' ? [x,y,z] : @key == '2' ? [y,z,x] : [x,z,y]
+		it.map{|t| t*SCALE}
+	end
+end
+
+class Euler
+	include Math
+
+	attr_reader :pts, :qts
+	def initialize num
+		@del_t = 0.002
+		@pts = points num
+		euler
+	end
+
+	def cent_rand
+		rand - 1 * rand
+	end
+
+	def update_points(pts)
+		@pts = pts
+	end
+
+	def points num
+		(1..num).map { [cent_rand, cent_rand]}
+	end
+
+	def diff(x,y)
+		# nonlinear oscillator
+		# b = 2 ; [	y, -b*y - Math.sin(x)]
+
+		# serpentine
+		# b= 0.1 ; k=cos(x*y) # x, y, z all good!
+		# [y, -x*k -b*y + PI*sin(y)]
+
+		# pendulum
+		# b = 0.3 ; [y,-b*y +sin(x)]
+		b = cos(x) ; [y,-b*y +sin(x)]
+
+		# huygens clocks, ie holy fucking hell 
+		# b = 1 ; k = 1 # x, y, z all good!
+		# [y, -x*k -b*y + 6]
+
+		# split up
+		# b = 2 ; k = 1.2*cos(x)
+		# [y, - x*k - b*y + PI*sin(y)]
+
+		# sun spots penumbra
+		# [cos(y*x),x/y]
+
+		# contracts and explands
+		# [y, -x/100]
+
+		# [y-x, x-y]
+	end
+
+	def euler
+		@qts = @pts.map do |x, y|
+			s, t = diff x, y
+			dx = x + s * @del_t
+			dy = y + t * @del_t
+
+			s, t = diff dx, dy
+			ddx = dx + s * @del_t
+			ddy = dy + t * @del_t
+
+			[(dx + ddx) /2.0, (dy + ddy) /2.0]
+		end
+	end
+end
+
+###############
+include Math
+include Torus
+require 'matrix'
+
+BODY_RESOLUTION = 5000.freeze
+ROTATION_Z = Matrix.rows([[0,sin(0.5*PI),cos(0.5*PI)],
+											    [0,cos(0.5*PI),sin(0.5*PI)],
+											    [1,0,0]]).freeze
+def setup
+	size displayWidth, displayHeight
+	@w, @h = width/2.0, height/2.0
+	colorMode(HSB,360,100,100)
+	background 0
+	frame_rate 40
+
+	# @all_coords = body_points BODY_RESOLUTION
+	# @all_coords.each do |mtrx|
+	# 	mtrx = Matrix.columns([mtrx])
+	# 	x_y_z = (ROTATION_Z * mtrx).to_a.flatten
+	# 	set_color(*x_y_z,mtrx)
+	# end
+	@step = 0
+	@offset = 300
+	@alpha = 0
+	stroke_width 1
+	@it = Euler.new 100#5000
+end
+
+def set_color(x,y,z,matrix)
+	a, b, c = matrix.to_a.flatten.map{|x| x/Tau }
+	color = [c < 0 ? 100 : 200, a < 0 ? 40 : 90 , 100]
+	set(x+@w, z+@h, color(*color))
+end
+
+def body_points(integer)
+	(0...integer).map do |x,y,z|
+		x = x/(integer.to_f) * PI
+		y = rand * PI
+		to_torus x, y
+	end
+end
+
+def color_shit
+	@step += 1
+	max_steps = 400
+	half_steps = max_steps / 2
+	alpha_increment = 1 / half_steps.to_f
+
+	if (@step > @offset)
+		offstep = @step - @offset
+		@alpha = 100 * (offstep < half_steps ? offstep * alpha_increment : (max_steps - offstep) * alpha_increment)
+	else
+		@alpha = 0
+	end
+	# puts [@step, @alpha]
+	return @step > @offset + max_steps
+end
+
+def draw
+	# clear
+	set_key
+	return if color_shit
+
+	@it.euler
+	pts = @it.pts
+	qts = @it.qts
+
+	pts.zip(qts).each do |(x,y),(s,t)|
+		x, y, z = to_torus(x,y)
+		s, t, r = to_torus(s,t)
+		stroke (x/4)%360, 80, 100, @alpha
+		line(x+@w, y+@h, s+@w, t+@h)
+	end
+
+	@it.update_points(qts)
+end
