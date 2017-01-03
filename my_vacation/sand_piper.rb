eccf314653a7c3f7e8b2fc51559d3d5a9553a634
Jonathan Zingale <jonzingale@gmail.com>
2017-01-03 01:43:25 -0800

    better sandpiper, better trigs

--- my_vacation/sand_piper.rb
+++ my_vacation/sand_piper.rb
@@ -1,17 +1,30 @@
   URL_STUB = '/Users/Jon/Desktop/crude/RubyProcessing/my_vacation/'
+  PI = 3.1415926
 
   class SandPiper
     def initialize(width, height)
       @w, @h = width, height
+      @r_ary = (0..3).map{|t| t/3.0}
       render_body
     end
 
-    def render_body(left=0, right=0)
-      stroke_width(6) ; stroke(120)
-      line(@w,@h+50,@w+left,@h+120) # left leg
+    def trigs(theta)
+      %w(cos sin).map{|s| Math.send(s, theta)}
+    end
 
-      no_stroke
-      fill(360)
+    def rotate(ary)
+      ary.unshift(ary.pop)
+    end
+
+    def render_body
+      r_sel = rotate(@r_ary).first
+      rando = PI/1.5 - PI * r_sel / 3
+      cos, sin = trigs(rando).map{|t| t*120}
+
+      stroke_width(6) ; stroke(120)
+      line(@w,@h+50,@w+cos,@h+sin) # left leg
+
+      no_stroke ; fill(360)
       ellipse(@w, @h, 130, 100) # body
       ellipse(@w-60, @h-30, 60, 60) # head
       bezier(@w-40, @h-30, @w+130, @h-10, @w+130, @h-30, @w+50, @h+10) # tail
@@ -21,22 +34,20 @@
       bezier(@w-89, @h-33, @w-169, @h, @w-159, @h, @w-89, @h-23) # beak
 
       stroke_width(6) ; stroke(120)
-      line(@w+10,@h+45,@w+20+right,@h+120) # right let
+      line(@w+10,@h+45,@w+20-cos,@h+sin) # right leg
     end
   end
 
   def setup
-    size(1280, 720)# displayHeight/2)
-    frame_rate 50
-
+    size(1280, 720) ; frame_rate 20
     colorMode(HSB,360,100,100,100)
     text_font create_font("SanSerif", 100)
-    @piper = SandPiper.new(width/3, height/2+200)
+    @piper = SandPiper.new(width/3, height/2+160)
     @img = loadImage(URL_STUB+'beach.jpg')
   end
 
   def draw
     image(@img, 0, 0)
-    text('My Winter Vacation',60,120)
-    @piper.render_body(rand(80)-40, rand(80)-40)
+    fill 80 ; text('My Winter Vacation',60,170)
+    @piper.render_body
   end
