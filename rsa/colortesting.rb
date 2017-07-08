8ec8948c825d57b7557bf6ee83dddf1e7ceae00d
Jonathan Zingale <jonzingale@gmail.com>
2017-07-07 18:11:22 -0600

    clean up

--- rsa/colortesting.rb
+++ rsa/colortesting.rb
@@ -28,6 +28,7 @@ def mouseMoved
   fill(180,100,100) ; text("#{hh.floor} #{ss.floor} #{bb.floor}", 15, 30)
 end
 
+# IMAGE SAVING AND LOADING
 IMAGES_PATH = File.expand_path('.', File.dirname(__FILE__)).freeze
 TMP = "#{IMAGES_PATH}/tester_tmp.jpg".freeze
 
@@ -36,4 +37,24 @@ def images
   save(TMP) ; clear
   loaded = loadImage(TMP)
   image(loaded,0,0)
+end
+
+# BLENDER
+CONV = PI / 180
+VNOC = 180 / PI
+TAU = 2 * PI
+
+def color_to_complex(color)
+  hh = hue(color) * CONV
+  ss = saturation(color) / 100.0
+  Complex(ss * cos(hh), ss * sin(hh))
+end
+
+def blend(colors)
+  cs = colors.map{|color| color_to_complex color}
+  avg = cs.inject(0, :+) / jt.length.to_f
+
+  hue = (avg.arg % TAU) * VNOC
+  sat = avg.abs * 100
+  [hue, sat].map(&:floor)
 end
